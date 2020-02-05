c4d3168700f5530c18d2e7bdfc3a1a0c3b651ca8
Derek Haynes <derek.haynes@gmail.com>
2020-02-05 14:57:21 -0700

    Ability to `pip install --editable .` so modules can be loaded outside this repo

--- consts/resource_paths.py
+++ consts/resource_paths.py
@@ -1,20 +1,23 @@
 import os
 from os import path
-
-device_cmd_fpath    = './depthai.cmd'
-
-prefix = './resources/'
-
-blob_labels_fpath = './resources/nn/object_detection_4shave/labels_for_mobilenet_ssd.txt'
-blob_fpath        = './resources/nn/object_detection_4shave/mobilenet_ssd.blob'
-blob_config_fpath = './resources/nn/object_detection_4shave/object_detection.json'
+from pathlib import Path
 
 
-if path.exists('./resources/depthai.calib'):
-    calib_fpath = './resources/depthai.calib'
+def relative_to_abs_path(relative_path):
+    dirname = Path(__file__).parent
+    return str((dirname / relative_path).resolve())
+
+prefix              = relative_to_abs_path('../resources/')+"/"
+device_cmd_fpath    = relative_to_abs_path('../depthai.cmd')
+custom_calib_fpath  = relative_to_abs_path('../resources/depthai.calib')
+blob_labels_fpath   = relative_to_abs_path('../resources/nn/object_detection_4shave/labels_for_mobilenet_ssd.txt')
+blob_fpath          = relative_to_abs_path('../resources/nn/object_detection_4shave/mobilenet_ssd.blob')
+blob_config_fpath   = relative_to_abs_path('../resources/nn/object_detection_4shave/object_detection.json')
+pipeline_config_fpath = relative_to_abs_path('../resources/config.json')
+
+if Path(custom_calib_fpath).exists():
+    calib_fpath = custom_calib_fpath
     print("Using Custom Calibration File: depthai.calib")
 else:
     calib_fpath = ''
     print("No calibration file. Using Calibration Defaults.")
-
-pipeline_config_fpath = './resources/config.json'
