445caf6a5e7f8434e54f7c06fa12b718bc7992ae
Derek Haynes <derek.haynes@gmail.com>
2020-02-17 11:26:53 -0700

    Adds --config-overwrite to test.py
    This adds the ability to overwrite the default config via a script argument. It works the same as the calibrate.py.
    
    Additionally:
    
    * Puts utils in a dedicated `depthai_helpers` folder
    * For utils shared by both calibrate and test, adds a depthai_helpers.utils module

---
+++ depthai_helpers/utils.py
@@ -0,0 +1,25 @@
+# https://stackoverflow.com/questions/20656135/python-deep-merge-dictionary-data#20666342
+def merge(source, destination):
+    """
+    run me with nosetests --with-doctest file.py
+
+    >>> a = { 'first' : { 'all_rows' : { 'pass' : 'dog', 'number' : '1' } } }
+    >>> b = { 'first' : { 'all_rows' : { 'fail' : 'cat', 'number' : '5' } } }
+    >>> merge(b, a) == { 'first' : { 'all_rows' : { 'pass' : 'dog', 'fail' : 'cat', 'number' : '5' } } }
+    True
+    """
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            merge(value, node)
+        else:
+            destination[key] = value
+
+    return destination
+def mkdir_overwrite(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    else:
+        shutil.rmtree(dir)
+        os.makedirs(dir)
