973714a5548a041d53760ebbd2c5e8a96989cf74
alex-luxonis <alex@luxonis.com>
2020-04-19 03:02:15 +0300

    Fix scripts for Python 3.5: path resolve() was failing for missing files

--- consts/resource_paths.py
+++ consts/resource_paths.py
@@ -5,7 +5,10 @@ from pathlib import Path
 
 def relative_to_abs_path(relative_path):
     dirname = Path(__file__).parent
-    return str((dirname / relative_path).resolve())
+    try:
+        return str((dirname / relative_path).resolve())
+    except FileNotFoundError:
+        return None
 
 prefix                = relative_to_abs_path('../resources/')+"/"
 device_cmd_fpath      = relative_to_abs_path('../depthai.cmd')
@@ -14,9 +17,8 @@ custom_calib_fpath    = relative_to_abs_path('../resources/depthai.calib')
 blob_labels_fpath     = relative_to_abs_path('../resources/nn/object_detection_4shave/labels_for_mobilenet_ssd.txt')
 blob_fpath            = relative_to_abs_path('../resources/nn/object_detection_4shave/mobilenet_ssd.blob')
 blob_config_fpath     = relative_to_abs_path('../resources/nn/object_detection_4shave/object_detection.json')
-pipeline_config_fpath = relative_to_abs_path('../resources/config.json')
 
-if Path(custom_calib_fpath).exists():
+if custom_calib_fpath is not None and Path(custom_calib_fpath).exists():
     calib_fpath = custom_calib_fpath
     print("Using Custom Calibration File: depthai.calib")
 else:
