7c4d92a9f46ad787d45b86bdea5b025ee7def71f
szabi-luxonis <szabi@luxonis.com>
2020-04-21 19:44:46 +0300

    add landmarks,age gender,emotion recognition examples: python3 depthai.py -cnn {CNN_model_name}
    Former-commit-id: 51ff8c66bbd9c4037fc3cc8828cd8334e07f6459

--- depthai.py
+++ depthai.py
@@ -79,6 +79,123 @@ def parse_args():
 
     return options
 
+def decode_mobilenet_ssd(nnet_packet):
+    detections = []
+    # the result of the MobileSSD has detection rectangles (here: entries), and we can iterate threw them
+    for _, e in enumerate(nnet_packet.entries()):
+        # for MobileSSD entries are sorted by confidence
+        # {id == -1} or {confidence == 0} is the stopper (special for OpenVINO models and MobileSSD architecture)
+        if e[0]['id'] == -1.0 or e[0]['confidence'] == 0.0:
+            break
+        # save entry for further usage (as image package may arrive not the same time as nnet package)
+        detections.append(e)
+    return detections
+
+def show_mobilenet_ssd(entries_prev, frame):
+    img_h = frame.shape[0]
+    img_w = frame.shape[1]
+    # iterate through pre-saved entries & draw rectangle & text on image:
+    for e in entries_prev:
+        # the lower confidence threshold - the more we get false positives
+        if e[0]['confidence'] > 0.5:
+            x1 = int(e[0]['left'] * img_w)
+            y1 = int(e[0]['top'] * img_h)
+
+            pt1 = x1, y1
+            pt2 = int(e[0]['right'] * img_w), int(e[0]['bottom'] * img_h)
+
+            cv2.rectangle(frame, pt1, pt2, (0, 0, 255))
+            # Handles case where TensorEntry object label is out if range
+            if e[0]['label'] > len(labels):
+                print("Label index=",e[0]['label'], "is out of range. Not applying text to rectangle.")
+            else:
+                pt_t1 = x1, y1 + 20
+                cv2.putText(frame, labels[int(e[0]['label'])], pt_t1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+                pt_t2 = x1, y1 + 40
+                cv2.putText(frame, '{:.2f}'.format(100*e[0]['confidence']) + ' %', pt_t2, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+                if config['ai']['calc_dist_to_bb']:
+                    pt_t3 = x1, y1 + 60
+                    cv2.putText(frame, 'x:' '{:7.3f}'.format(e[0]['distance_x']) + ' m', pt_t3, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+
+                    pt_t4 = x1, y1 + 80
+                    cv2.putText(frame, 'y:' '{:7.3f}'.format(e[0]['distance_y']) + ' m', pt_t4, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+
+                    pt_t5 = x1, y1 + 100
+                    cv2.putText(frame, 'z:' '{:7.3f}'.format(e[0]['distance_z']) + ' m', pt_t5, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+    return frame
+
+def decode_age_gender_recognition(nnet_packet):
+    detections = []
+    for _, e in enumerate(nnet_packet.entries()):
+        detections.append(e[0]["age"])
+        if e[1]["female"] > e[1]["male"]:
+            detections.append("female")
+        else:
+            detections.append("male")
+    return detections
+
+def show_age_gender_recognition(entries_prev, frame):
+    # img_h = frame.shape[0]
+    # img_w = frame.shape[1]
+    if len(entries_prev) != 0:
+        age = (int)(entries_prev[0]*100)
+        cv2.putText(frame, "Age: " + str(age), (0, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        gender = entries_prev[1]
+        cv2.putText(frame, "G: " + str(gender), (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    frame = cv2.resize(frame, (300, 300))
+    return frame
+
+def decode_emotion_recognition(nnet_packet):
+    detections = []
+    for i in range(len(nnet_packet.entries()[0][0])):
+        detections.append(nnet_packet.entries()[0][0][i])
+    return detections
+
+def show_emotion_recognition(entries_prev, frame):
+    # img_h = frame.shape[0]
+    # img_w = frame.shape[1]
+    e_states = {
+        0 : "neutral",
+        1 : "happy",
+        2 : "sad",
+        3 : "surprise",
+        4 : "anger"
+    }
+    if len(entries_prev) != 0:
+        emotion = e_states[np.argmax(entries_prev)]
+        cv2.putText(frame, emotion, (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    frame = cv2.resize(frame, (300, 300))
+
+    return frame
+
+
+def decode_landmarks_recognition(nnet_packet):
+    landmarks = []
+    for i in range(len(nnet_packet.entries()[0][0])):
+        landmarks.append(nnet_packet.entries()[0][0][i])
+    
+    landmarks = list(zip(*[iter(landmarks)]*2))
+    return landmarks
+
+def show_landmarks_recognition(entries_prev, frame):
+    img_h = frame.shape[0]
+    img_w = frame.shape[1]
+
+    if len(entries_prev) != 0:
+        for i in entries_prev:
+            try:
+                x = int(i[0]*img_h)
+                y = int(i[1]*img_w)
+            except:
+                continue
+            # # print(x,y)
+            cv2.circle(frame, (x,y), 3, (0, 0, 255))
+
+    frame = cv2.resize(frame, (300, 300))
+
+    return frame
+
 global args
 try:
     args = vars(parse_args())
@@ -107,6 +224,24 @@ calc_dist_to_bb = True
 if args['disable_depth']:
     calc_dist_to_bb = False
 
+decode_nn=decode_mobilenet_ssd
+show_nn=show_mobilenet_ssd
+
+if args['cnn_model'] == 'age-gender-recognition-retail-0013':
+    decode_nn=decode_age_gender_recognition
+    show_nn=show_age_gender_recognition
+    calc_dist_to_bb=False
+
+if args['cnn_model'] == 'emotions-recognition-retail-0003':
+    decode_nn=decode_emotion_recognition
+    show_nn=show_emotion_recognition
+    calc_dist_to_bb=False
+
+if args['cnn_model'] in ['facial-landmarks-35-adas-0002', 'landmarks-regression-retail-0009']:
+    decode_nn=decode_landmarks_recognition
+    show_nn=show_landmarks_recognition
+    calc_dist_to_bb=False
+
 if args['cnn_model']:
     cnn_model_path = consts.resource_paths.nn_resource_path + args['cnn_model']+ "/" + args['cnn_model']
     blob_file = cnn_model_path + ".blob"
@@ -118,8 +253,10 @@ if args['cnn_model']:
 with open(blob_file_config) as f:
     data = json.load(f)
 
-labels = data['mappings']['labels']
-
+try:
+    labels = data['mappings']['labels']
+except:
+    print("Labels not found in json!")
 
 
 print('depthai.__version__ == %s' % depthai.__version__)
@@ -224,19 +361,8 @@ while True:
             print("process watchdog timeout")
             os._exit(10)
 
-    for i, nnet_packet in enumerate(nnet_packets):
-        # the result of the MobileSSD has detection rectangles (here: entries), and we can iterate threw them
-        for i, e in enumerate(nnet_packet.entries()):
-            # for MobileSSD entries are sorted by confidence
-            # {id == -1} or {confidence == 0} is the stopper (special for OpenVINO models and MobileSSD architecture)
-            if e[0]['id'] == -1.0 or e[0]['confidence'] == 0.0:
-                break
-
-            if i == 0:
-                entries_prev.clear()
-
-            # save entry for further usage (as image package may arrive not the same time as nnet package)
-            entries_prev.append(e)
+    for _, nnet_packet in enumerate(nnet_packets):
+        entries_prev = decode_nn(nnet_packet)
 
     for packet in data_packets:
         if packet.stream_name not in stream_names:
@@ -250,41 +376,9 @@ while True:
             data2 = data[2,:,:]
             frame = cv2.merge([data0, data1, data2])
 
-            img_h = frame.shape[0]
-            img_w = frame.shape[1]
-
-            # iterate threw pre-saved entries & draw rectangle & text on image:
-            for e in entries_prev:
-                # the lower confidence threshold - the more we get false positives
-                if e[0]['confidence'] > 0.5:
-                    x1 = int(e[0]['left'] * img_w)
-                    y1 = int(e[0]['top'] * img_h)
-
-                    pt1 = x1, y1
-                    pt2 = int(e[0]['right'] * img_w), int(e[0]['bottom'] * img_h)
-
-                    cv2.rectangle(frame, pt1, pt2, (0, 0, 255))
-                    # Handles case where TensorEntry object label = 7552.
-                    if e[0]['label'] > len(labels):
-                        print("Label index=",e[0]['label'], "is out of range. Not applying text to rectangle.")
-                    else:
-                        pt_t1 = x1, y1 + 20
-                        cv2.putText(frame, labels[int(e[0]['label'])], pt_t1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-                        pt_t2 = x1, y1 + 40
-                        cv2.putText(frame, '{:.2f}'.format(100*e[0]['confidence']) + ' %', pt_t2, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-                        if config['ai']['calc_dist_to_bb']:
-                            pt_t3 = x1, y1 + 60
-                            cv2.putText(frame, 'x:' '{:7.3f}'.format(e[0]['distance_x']) + ' m', pt_t3, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-
-                            pt_t4 = x1, y1 + 80
-                            cv2.putText(frame, 'y:' '{:7.3f}'.format(e[0]['distance_y']) + ' m', pt_t4, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-
-                            pt_t5 = x1, y1 + 100
-                            cv2.putText(frame, 'z:' '{:7.3f}'.format(e[0]['distance_z']) + ' m', pt_t5, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-
-            cv2.putText(frame, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
-            cv2.imshow('previewout', frame)
+            nn_frame = show_nn(entries_prev, frame)
+            cv2.putText(nn_frame, "fps: " + str(frame_count_prev[packet.stream_name]), (25, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
+            cv2.imshow('previewout', nn_frame)
         elif packet.stream_name == 'left' or packet.stream_name == 'right' or packet.stream_name == 'disparity':
             frame_bgr = packet.getData()
             cv2.putText(frame_bgr, packet.stream_name, (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
