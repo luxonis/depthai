736fcc24de9db847f242e86cf06cecbd501dd1c7
szabi-luxonis <szabi@luxonis.com>
2020-05-05 16:40:30 +0300

    add configurable depth confidence threshold; remove limit for padding

--- depthai.py
+++ depthai.py
@@ -376,6 +376,7 @@ config = {
         'calibration_file': consts.resource_paths.calib_fpath,
         'padding_factor': 0.3,
         'depth_limit_m': 10.0, # In meters, for filtering purpose during x,y,z calc
+        'confidence_threshold' : 0.5, #Depth is calculated for bounding boxes with confidence higher than this number 
     },
     'ai':
     {
