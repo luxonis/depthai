913e36f572029cdc021e093f1642da1f619be63c
Richmond Hollen <richmond.hollen@ouster.io>
2020-05-12 19:45:42 -0700

    Add test case for cli_print function

--- tests/test_cli_utils.py
+++ tests/test_cli_utils.py
@@ -3,7 +3,7 @@ import unittest
 from argparse import Namespace
 import numpy as np
 
-from depthai_helpers.cli_utils import parse_args
+from depthai_helpers.cli_utils import parse_args, cli_print, PrintColors
 
 
 class TestCliUtils(unittest.TestCase):
@@ -16,3 +16,6 @@ class TestCliUtils(unittest.TestCase):
         assert np.isclose(options.rgb_field_of_view, 68.7938)
         assert np.isclose(options.rgb_baseline, 2.0)
         assert options.swap_lr is True
+
+    def test_cli_print_valid(self):
+        cli_print("some message", PrintColors.WARNING)
