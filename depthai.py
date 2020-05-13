fa0405c0b73d5dd44914c36edd420d4c91820da4
Richmond Hollen <richmond.hollen@ouster.io>
2020-05-12 19:18:48 -0700

    Move PrintColors class to cli_utils module

--- depthai.py
+++ depthai.py
@@ -17,16 +17,6 @@ import depthai
 import consts.resource_paths
 from depthai_helpers import utils
 
-class PrintColors:
-    HEADER = '\033[95m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    RED = '\033[91m'
-    WARNING = '\033[1;5;31m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 def parse_args():
     epilog_text = '''
---
+++ depthai_helpers/cli_utils.py
@@ -0,0 +1,12 @@
+
+
+class PrintColors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    WARNING = '\033[1;5;31m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
