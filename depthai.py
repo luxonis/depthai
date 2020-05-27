eeb7bdba2668ed7840311b575c432911af1a9d84
Richmond Hollen <richmond.g.hollen@gmail.com>
2020-05-27 11:25:55 -0700

    Add cli utils (#106)
    * Remove unused imports
    
    * Reorder imports:  builtins, third-party, internal
    
    * Rename colors used for print statements
    
    * Move PrintColors class to cli_utils module
    
    * Move parse_args function to cli_utils module
    
    * Move decode function back to main depthai module
    
    * Add PEP8 autoformatting
    
    * Simplify logic for checking options and setting defaults
    
    * Add error messages to exceptions in stream_type function
    
    * Make all single quotes double quotes in cli_utils module
    
    * Add test for cli utils function
    
    * Add cli_print function to stop repeating code
    
    * Change print colors to enum type
    
    * Add test case for cli_print function
    
    * Change all prints with colors to cli_print function
    
    * Add test to make sure input is checked for print_cli
    
    * Add if/main block to run all unit tests in the script
    
    Co-authored-by: Richmond Hollen <richmond.hollen@ouster.io>

--- depthai.py
+++ depthai.py
@@ -1,136 +1,21 @@
 #!/usr/bin/env python3
 
-import sys
-from time import monotonic
-from time import time
-from time import sleep
-import argparse
-from argparse import ArgumentParser
-from pathlib import Path
 import json
-import numpy as np
-import cv2
+from pathlib import Path
+import platform
 import os
 import subprocess
-import platform
-from pathlib import Path
+from time import time, sleep
+
+import cv2
+import numpy as np
 
 import depthai
 
 import consts.resource_paths
 from depthai_helpers import utils
+from depthai_helpers.cli_utils import cli_print, parse_args, PrintColors
 
-class bcolors:
-    HEADER = '\033[95m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    RED = '\033[91m'
-    WARNING = '\033[1;5;31m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-def parse_args():
-    epilog_text = '''
-    Displays video streams captured by DepthAI.
-
-    Example usage:
-
-    ## Show the depth stream:
-    python3 test.py -s depth_sipp,12
-    ## Show the depth stream and NN output:
-    python3 test.py -s metaout previewout,12 depth_sipp,12
-    '''
-    parser = ArgumentParser(epilog=epilog_text,formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-co", "--config_overwrite", default=None,
-                        type=str, required=False,
-                        help="JSON-formatted pipeline config object. This will be override defaults used in this script.")
-    parser.add_argument("-brd", "--board", default=None, type=str,
-                        help="BW1097, BW1098OBC - Board type from resources/boards/ (not case-sensitive). "
-                            "Or path to a custom .json board config. Mutually exclusive with [-fv -rfv -b -r -w]")
-    parser.add_argument("-fv", "--field-of-view", default=None, type=float,
-                        help="Horizontal field of view (HFOV) for the stereo cameras in [deg]. Default: 71.86deg.")
-    parser.add_argument("-rfv", "--rgb-field-of-view", default=None, type=float,
-                        help="Horizontal field of view (HFOV) for the RGB camera in [deg]. Default: 68.7938deg.")
-    parser.add_argument("-b", "--baseline", default=None, type=float,
-                        help="Left/Right camera baseline in [cm]. Default: 9.0cm.")
-    parser.add_argument("-r", "--rgb-baseline", default=None, type=float,
-                        help="Distance the RGB camera is from the Left camera. Default: 2.0cm.")
-    parser.add_argument("-w", "--no-swap-lr", dest="swap_lr", default=None, action="store_false",
-                        help="Do not swap the Left and Right cameras.")
-    parser.add_argument("-e", "--store-eeprom", default=False, action='store_true',
-                        help="Store the calibration and board_config (fov, baselines, swap-lr) in the EEPROM onboard")
-    parser.add_argument("--clear-eeprom", default=False, action='store_true',
-                        help="Invalidate the calib and board_config from EEPROM")
-    parser.add_argument("-o", "--override-eeprom", default=False, action='store_true',
-                        help="Use the calib and board_config from host, ignoring the EEPROM data if programmed")
-    parser.add_argument("-dev", "--device-id", default='', type=str,
-                        help="USB port number for the device to connect to. Use the word 'list' to show all devices and exit.")
-    parser.add_argument("-debug", "--dev_debug", default=None, action='store_true',
-                        help="Used by board developers for debugging.")
-    parser.add_argument("-fusb2", "--force_usb2", default=None, action='store_true',
-                        help="Force usb2 connection")
-    parser.add_argument("-cnn", "--cnn_model", default='mobilenet-ssd', type=str, 
-                        help="Cnn model to run on DepthAI")
-    parser.add_argument("-dd", "--disable_depth", default=False,  action='store_true', 
-                        help="Disable depth calculation on CNN models with bounding box output")
-    parser.add_argument("-bb", "--draw-bb-depth", default=False,  action='store_true',
-                        help="Draw the bounding boxes over the left/right/depth* streams")
-    parser.add_argument("-ff", "--full-fov-nn", default=False,  action='store_true',
-                        help="Full RGB FOV for NN, not keeping the aspect ratio")
-    parser.add_argument("-s", "--streams",  
-                        nargs='+',
-                        type=stream_type,
-                        dest='streams',
-                        default=['metaout', 'previewout'],
-                        help="Define which streams to enable \
-                        Format: stream_name or stream_name,max_fps \
-                        Example: -s metaout previewout \
-                        Example: -s metaout previewout,10 depth_sipp,10")
-    parser.add_argument("-v", "--video", default=None, type=str, required=False, help="Path where to save video stream (existing file will be overwritten)")
-
-    options = parser.parse_args()
-
-    if (options.board is not None) and ((options.field_of_view     is not None)
-                                     or (options.rgb_field_of_view is not None)
-                                     or (options.baseline          is not None)
-                                     or (options.rgb_baseline      is not None)
-                                     or (options.swap_lr           is not None)):
-        parser.error("[-brd] is mutually exclusive with [-fv -rfv -b -r -w]")
-
-    # Set some defaults after the above check
-    if options.field_of_view     is None: options.field_of_view = 71.86
-    if options.rgb_field_of_view is None: options.rgb_field_of_view = 68.7938
-    if options.baseline          is None: options.baseline = 9.0
-    if options.rgb_baseline      is None: options.rgb_baseline = 2.0
-    if options.swap_lr           is None: options.swap_lr = True
-
-    return options
-
-def stream_type(option):
-    option_list = option.split(',')
-    option_args = len(option_list)
-    if option_args not in [1,2]:
-        print(bcolors.WARNING + option+" format is invalid. See --help" + bcolors.ENDC)
-        raise ValueError
-
-    stream_choices=['metaout', 'previewout', 'left', 'right', 'depth_sipp', 'disparity', 'depth_color_h', 'meta_d2h']
-    stream_name = option_list[0]
-    if stream_name not in stream_choices:
-        print(bcolors.WARNING + stream_name +" is not in available stream list: \n" + str(stream_choices) + bcolors.ENDC)
-        raise ValueError
-
-    if(option_args == 1):
-        stream_dict = {'name': stream_name}
-    else:       
-        try:
-            max_fps = float(option_list[1])
-        except:
-            print(bcolors.WARNING + "In option: " + str(option) + " " + option_list[1] + " is not a number!" + bcolors.ENDC)
-
-        stream_dict = {'name': stream_name, "max_fps": max_fps}
-    return stream_dict
 
 def decode_mobilenet_ssd(nnet_packet):
     detections = []
@@ -143,6 +28,7 @@ def decode_mobilenet_ssd(nnet_packet):
         # save entry for further usage (as image package may arrive not the same time as nnet package)
         detections.append(e)
     return detections
+
 
 def nn_to_depth_coord(x, y):
     x_depth = int(nn2depth['off_x'] + x * nn2depth['max_w'])
@@ -289,7 +175,7 @@ if args['config_overwrite']:
 print("Using Arguments=",args)
 
 if args['force_usb2']:
-    print(bcolors.WARNING + "FORCE USB2 MODE" + bcolors.ENDC)
+    cli_print("FORCE USB2 MODE", PrintColors.WARNING)
     cmd_file = consts.resource_paths.device_usb2_cmd_fpath
 else:
     cmd_file = consts.resource_paths.device_cmd_fpath
@@ -331,11 +217,11 @@ if args['cnn_model']:
 blob_file_path = Path(blob_file)
 blob_file_config_path = Path(blob_file_config)
 if not blob_file_path.exists():
-    print(bcolors.WARNING + "\nWARNING: NN blob not found in: " + blob_file + bcolors.ENDC)
+    cli_print("\nWARNING: NN blob not found in: " + blob_file, PrintColors.WARNING)
     os._exit(1)
 
 if not blob_file_config_path.exists():
-    print(bcolors.WARNING + "\nWARNING: NN json not found in: " + blob_file_config + bcolors.ENDC)
+    cli_print("\nWARNING: NN json not found in: " + blob_file_config, PrintColors.WARNING)
     os._exit(1)
 
 with open(blob_file_config) as f:
@@ -353,11 +239,11 @@ print('depthai.__dev_version__ == %s' % depthai.__dev_version__)
 if platform.system() == 'Linux':
     ret = subprocess.call(['grep', '-irn', 'ATTRS{idVendor}=="03e7"', '/etc/udev/rules.d'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     if(ret != 0):
-        print(bcolors.WARNING + "\nWARNING: Usb rules not found" + bcolors.ENDC)
-        print(bcolors.RED + "\nSet rules: \n" \
-        """echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules \n""" \
-        "sudo udevadm control --reload-rules && udevadm trigger \n" \
-        "Disconnect/connect usb cable on host! \n"    + bcolors.ENDC)
+        cli_print("\nWARNING: Usb rules not found", PrintColors.WARNING)
+        cli_print("\nSet rules: \n"
+        """echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules \n"""
+        "sudo udevadm control --reload-rules && udevadm trigger \n"
+        "Disconnect/connect usb cable on host! \n", PrintColors.RED)
         os._exit(1)
 
 if not depthai.init_device(cmd_file, args['device_id']):
