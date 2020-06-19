31dec104414538bdd9ae316d4197bc295e7cfc1e
szabi-luxonis <szabi@luxonis.com>
2020-06-19 23:51:27 +0300

    Add cloud compile script for NN blob; add configurable camera config
    Former-commit-id: ae82f25ceff685f0bc581b80acae1c6771acbc61

--- depthai_helpers/cli_utils.py
+++ depthai_helpers/cli_utils.py
@@ -44,6 +44,14 @@ def parse_args():
     parser.add_argument("-brd", "--board", default=None, type=str,
                         help="BW1097, BW1098OBC - Board type from resources/boards/ (not case-sensitive). "
                              "Or path to a custom .json board config. Mutually exclusive with [-fv -rfv -b -r -w]")
+    parser.add_argument("-sh", "--shaves", default=4, type=int,
+                        help="Number of shaves used by NN.")
+    parser.add_argument("-cmx", "--cmx_slices", default=4, type=int,
+                        help="Number of cmx slices used by NN.")
+    parser.add_argument("-NCE", "--NCEs", default=2, type=int,
+                        help="Number of NCEs used by NN.")
+    parser.add_argument("-rgb", "--rgb_cam_config", default="1080p_30hz", type=str,
+                        help="RGB cam config: 1080p_30hz or 4k_30hz are supported.")
     parser.add_argument("-fv", "--field-of-view", default=None, type=float,
                         help="Horizontal field of view (HFOV) for the stereo cameras in [deg]. Default: 71.86deg.")
     parser.add_argument("-rfv", "--rgb-field-of-view", default=None, type=float,
--- model_compiler/download_and_compile.sh
+++ model_compiler/download_and_compile.sh
@@ -1,81 +1,45 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 
 OPENVINO_VERSION="2020.1.023"
 
 echo_and_run() { echo -e "\$ $* \n" ; "$@" ; }
 
-if [ $1 = "CLOUD_COMPILE" ] 
-then
+MODEL_NAME=$1
+SHAVE_NR=$2
+CMX_NR=$3
+
 CLOUD_COMPILE="yes"
-fi
 
 CUR_DIR=$PWD
-MODELS_LIST=`realpath models.txt`
 DOWNLOADS_DIR=`realpath downloads`
 NN_PATH=`realpath ../resources/nn`
-OPENVINO_PATH=`realpath /opt/intel/openvino_$OPENVINO_VERSION`
 
-if [ ! -d "$OPENVINO_PATH" ]; then
-echo "OPENVINO_PATH doesn't exist! Openvino $OPENVINO_VERSION is not installed?"
-exit 1
+MODEL_DOWNLOADER_OPTIONS="--precisions FP16 --output_dir $DOWNLOADS_DIR --cache_dir $DOWNLOADS_DIR --num_attempts 5 --name $MODEL_NAME"
+MODEL_DOWNLOADER_PATH=`realpath open_model_zoo/tools/downloader/downloader.py`
+
+BLOB_SUFFIX='.sh'$SHAVE_NR'cmx'$CMX_NR
+
+BLOB_OUT=$NN_PATH/$MODEL_NAME/$MODEL_NAME.blob$BLOB_SUFFIX
+
+if [ -f "$BLOB_OUT" ]; then
+    echo "$MODEL_NAME.blob$BLOB_SUFFIX already compiled."
+    exit 0
 fi
 
-if [ ! -d "$NN_PATH" ]; then
-echo "NN_PATH doesn't exist"
-exit 2
-fi
-
-source $OPENVINO_PATH/bin/setupvars.sh
-
-SHAVE_NR='4'
-CMX_NR='4'
-
-
-MODEL_DOWNLOADER_OPTIONS="--precisions FP16 --output_dir $DOWNLOADS_DIR --cache_dir $DOWNLOADS_DIR --num_attempts 5 --list $MODELS_LIST"
-MODEL_DOWNLOADER_PATH="$OPENVINO_PATH/deployment_tools/tools/model_downloader/downloader.py"
-
-
-MYRIAD_COMPILE_OPTIONS="-ip U8 -VPU_MYRIAD_PLATFORM VPU_MYRIAD_2480 -VPU_NUMBER_OF_SHAVES $SHAVE_NR -VPU_NUMBER_OF_CMX_SLICES $CMX_NR"
-MYRIAD_COMPILE_PATH="$OPENVINO_PATH/deployment_tools/inference_engine/lib/intel64/myriad_compile"
 
 CLOUD_COMPILE_SCRIPT=$PWD/model_converter.py
 
-rm -f $MODELS_LIST
+
 
 cd $NN_PATH
-for f in *; do
-    if [ -d "$f" ]; then
-        # $f is a directory
-        echo "$f" >> $MODELS_LIST
+if [ -d "$MODEL_NAME" ]; then
+    echo_and_run python3 $CLOUD_COMPILE_SCRIPT -i $MODEL_NAME -o $BLOB_OUT --shaves $SHAVE_NR --cmx_slices $CMX_NR 
+    if [ $? != 0 ]; then
+        rm -f $BLOB_OUT
+        exit 2
     fi
-done
+fi
 
-
-echo_and_run python3 $MODEL_DOWNLOADER_PATH $MODEL_DOWNLOADER_OPTIONS
-
-
-cd $DOWNLOADS_DIR/public
-for f in *; do
-    if [ -d "$f" ]; then
-        # $f is a directory
-        mkdir $DOWNLOADS_DIR/intel/$f/FP16/
-        echo_and_run $OPENVINO_PATH/deployment_tools/model_optimizer/mo.py --input_model $DOWNLOADS_DIR/public/$f/$f.caffemodel --input_proto $DOWNLOADS_DIR/public/$f/$f.prototxt --data_type=FP16 --mean_values [127.5,127.5,127.5] --scale_values [255,255,255] -o $DOWNLOADS_DIR/intel/$f/FP16/
-    fi
-done
-
-cd $NN_PATH
-for f in *; do
-    if [ -d "$f" ]; then
-        # $f is a directory
-        if [ "$CLOUD_COMPILE" = "yes" ] 
-        then
-            echo_and_run python3 $CLOUD_COMPILE_SCRIPT --xml $DOWNLOADS_DIR/intel/$f/FP16/$f.xml --bin $DOWNLOADS_DIR/intel/$f/FP16/$f.bin -o $NN_PATH/$f/$f.blob
-        else
-            echo_and_run $MYRIAD_COMPILE_PATH $MYRIAD_COMPILE_OPTIONS -m $DOWNLOADS_DIR/intel/$f/FP16/$f.xml -o $NN_PATH/$f/$f.blob
-        fi
-    fi
-done
 
 cd $CUR_DIR
-
-rm -f $MODELS_LIST
