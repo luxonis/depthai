ca4d91f5b587af46e95aafc6ccb74a8e2f456f37
alex-luxonis <alex@luxonis.com>
2020-07-07 02:53:36 +0300

    Implement configurable FPS for RGB: max 42 fps for 4K, max 118 fps 1080p. Higher is possible by tweaking the configs.
    Former-commit-id: c7821cd2798bff3fbe7ec473b46373332e1e0551

--- depthai_helpers/cli_utils.py
+++ depthai_helpers/cli_utils.py
@@ -51,13 +51,13 @@ def parse_args():
     parser.add_argument("-nce", "--NN_engines", default=None, type=int,
                         help="Number of NN_engines used by NN.")
     parser.add_argument("-rgbr", "--rgb_resolution", default=1080, type=int,
-                        help="RGB cam res config: 1080 or 2160 are supported.")
-    parser.add_argument("-rgbf", "--rgb_fps", default=30, type=int,
-                        help="RGB cam fps config: 30 fps is supported.")
+                        help="RGB cam res height: (1920x)1080 or (3840x)2160. Default: %(default)s")
+    parser.add_argument("-rgbf", "--rgb_fps", default=30.0, type=float,
+                        help="RGB cam fps: max 118.0 for H:1080, max 42.0 for H:2160. Default: %(default)s")
     parser.add_argument("-monor", "--mono_resolution", default=720, type=int,
                         help="Mono cam res height: (1280x)720, (1280x)800 or (640x)400 - binning. Default: %(default)s")
     parser.add_argument("-monof", "--mono_fps", default=30.0, type=float,
-                        help="Mono cam fps: 1.0..60.0 for 720 or 800, 1.0..120.0 for 400. Default: %(default)s")
+                        help="Mono cam fps: max 60.0 for H:720 or H:800, max 120.0 for H:400. Default: %(default)s")
     parser.add_argument("-fv", "--field-of-view", default=None, type=float,
                         help="Horizontal field of view (HFOV) for the stereo cameras in [deg]. Default: 71.86deg.")
     parser.add_argument("-rfv", "--rgb-field-of-view", default=None, type=float,
