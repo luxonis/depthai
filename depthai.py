2d94777887fe5950a11f0d80100dc6b0eb319341
rt-luxonis <rares@luxonis.com>
2020-08-08 13:35:25 +0300

    op mods

--- depthai.py
+++ depthai.py
@@ -74,14 +74,14 @@ if args['cnn_model'] == 'openpose':
     decode_nn=decode_openpose
     show_nn=show_openpose
     calc_dist_to_bb=False
-    compile_model=False
+#    compile_model=True
     
 if args['cnn_model'] == 'openpose2':
     from depthai_helpers.openpose2_handler import decode_openpose, show_openpose
     decode_nn=decode_openpose
     show_nn=show_openpose
     calc_dist_to_bb=False
-    compile_model=False
+    compile_model=True
 
 if args['cnn_model'] in ['facial-landmarks-35-adas-0002', 'landmarks-regression-retail-0009']:
     from depthai_helpers.landmarks_recognition_handler import decode_landmarks_recognition, show_landmarks_recognition
