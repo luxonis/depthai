4d3fe31aa43ccafb80368dc04268885cae95f270
GergelySzabolcs <szabi@luxonis.com>
2020-09-10 19:22:55 +0300

    Disable temporarily left-right check option for SGBM
    Former-commit-id: 9e796e3fb88f4aed4e5e1173c141d5836b68682a

--- depthai_helpers/arg_manager.py
+++ depthai_helpers/arg_manager.py
@@ -105,8 +105,8 @@ class CliArgs:
         parser.add_argument("-med", "--stereo_median_size", default=7, type=int, choices=[0, 3, 5, 7],
                             help="Disparity / depth median filter kernel size (N x N) . 0 = filtering disabled. Default: %(default)s")
 
-        parser.add_argument("-lrc", "--stereo_lr_check", default=False, action="store_true",
-                        help="Enable stereo 'Left-Right check' feature.")
+        # parser.add_argument("-lrc", "--stereo_lr_check", default=False, action="store_true",
+        #                 help="Enable stereo 'Left-Right check' feature.")
         parser.add_argument("-fv", "--field-of-view", default=None, type=float,
                             help="Horizontal field of view (HFOV) for the stereo cameras in [deg]. Default: 71.86deg.")
 
