ecb05d15f3c8e3ea3cf433c248f1e9dbb232011f
SzabolcsGergely <szabi@luxonis.com>
2020-09-24 07:33:22 +0300

    Simplify decoding for age-gender

--- depthai_helpers/age_gender_recognition_handler.py
+++ depthai_helpers/age_gender_recognition_handler.py
@@ -3,19 +3,9 @@ import numpy as np
 import json
 
 def decode_age_gender_recognition(nnet_packet, **kwargs):
-    return nnet_packet
-
-def decode_age_gender_recognition_json(nnet_packet, **kwargs):
-    detections = decode_age_gender_recognition(nnet_packet, **kwargs)
-    return json.dumps(detections)
-
-def show_age_gender_recognition(entries_prev, frame, **kwargs):
-    # img_h = frame.shape[0]
-    # img_w = frame.shape[1]
-
     config = kwargs['config']
 
-    output_list = entries_prev.getOutputsList()
+    output_list = nnet_packet.getOutputsList()
 
     age_out = output_list[0]
     age = age_out[0,0,0,0]
@@ -23,12 +13,27 @@ def show_age_gender_recognition(entries_prev, frame, **kwargs):
     female_conf = gender_out[0,0,0,0]
     male_conf = gender_out[0,1,0,0]
 
+    detection = None
+
     conf_thr = config['depth']['confidence_threshold']
     if female_conf > conf_thr or male_conf > conf_thr:
         gender = "male"
         if female_conf > male_conf:
             gender = "female"
         age = (int)(age*100)
+        detection = dict(gender=gender, age=age)
+    
+    return detection
+
+def decode_age_gender_recognition_json(nnet_packet, **kwargs):
+    detections = decode_age_gender_recognition(nnet_packet, **kwargs)
+    return json.dumps(detections)
+
+def show_age_gender_recognition(decoded_nn, frame, **kwargs):
+
+    if decoded_nn is not None:
+        gender = decoded_nn["gender"]
+        age = decoded_nn["age"]
         cv2.putText(frame, "Age: " + str(age), (0, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         cv2.putText(frame, "G: " + str(gender), (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     
