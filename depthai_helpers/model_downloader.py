e40ea5ebdcc3c9c485dd430f695839a14e758aa6
SzabolcsGergely <szabi@luxonis.com>
2020-10-15 22:08:47 +0300

    Add model zoo w/ model downloader and compiler; remove all blobs

--- depthai_helpers/model_downloader.py
+++ depthai_helpers/model_downloader.py
@@ -1,30 +1,42 @@
 import requests
+import subprocess
+import numpy as np
+from pathlib import Path
 
+from consts.resource_paths import nn_resource_path
+from consts.resource_paths import model_downloader_path, download_folder_path
+from consts.resource_paths import model_compiler_path
 
 def download_model(model, shaves, cmx_slices, nces, output_file):
-    PLATFORM="VPU_MYRIAD_2450" if nces == 0 else "VPU_MYRIAD_2480"
 
-    url = "http://luxonis.com:8080/"
-    payload = {
-        'compile_type': 'zoo',
-        'model_name': model,
-        'model_downloader_params': '--precisions FP16 --num_attempts 5',
-        'intermediate_compiler_params': '--data_type=FP16 --mean_values [127.5,127.5,127.5] --scale_values [255,255,255]',
-        'compiler_params': '-ip U8 -VPU_MYRIAD_PLATFORM ' + PLATFORM + ' -VPU_NUMBER_OF_SHAVES ' + str(shaves) +' -VPU_NUMBER_OF_CMX_SLICES ' + str(cmx_slices)
-    }
-    try:
-        response = requests.request("POST", url, data=payload)
-    except:
-        print("Connection timed out!")
-        return 1
+    model_downloader_options=f"--precisions FP16 --output_dir {download_folder_path} --cache_dir {download_folder_path}/.cache --num_attempts 5 --name {model} --model_root {nn_resource_path}"
+    model_downloader_options = model_downloader_options.split()
+    downloader_cmd = ["python3", f"{model_downloader_path}"]
+    downloader_cmd = np.concatenate((downloader_cmd, model_downloader_options))
+    # print(downloader_cmd)
+    result = subprocess.run(downloader_cmd)
+    if result.returncode != 0:
+        raise RuntimeError("Model downloader failed! Not connected to the internet?")
     
-    if response.status_code == 200:
-        blob_file = open(output_file, 'wb')
-        blob_file.write(response.content)
-        blob_file.close()
-    else:
-        print("Model compilation failed with error code: " + str(response.status_code))
-        print(str(response.text.encode('utf8')))
-        return 2
+    download_location = Path(download_folder_path) / model / "FP16"
+    if(not download_location.exists()):
+        raise RuntimeError(f"{download_location} doesn't exist for downloaded model!")
+    xml_path = download_location / (model + ".xml")
+    if(not xml_path.exists()):
+        raise RuntimeError(f"{xml_path} doesn't exist for downloaded model!")
+    bin_path = download_location / (model + ".bin")
+    if(not bin_path.exists()):
+        raise RuntimeError(f"{bin_path} doesn't exist for downloaded model!")
+
+
+    output_location = Path(nn_resource_path) / model / output_file
+    model_compiler_options=f"--xml {xml_path} --bin {bin_path} --output {output_location} --shaves {shaves} --cmx_slices {cmx_slices} --NCE {nces}"
+    model_compiler_options = model_compiler_options.split()
+    compiler_cmd = ["python3", f"{model_compiler_path}"]
+    compiler_cmd = np.concatenate((compiler_cmd, model_compiler_options))
+    # print(compiler_cmd)
+    result = subprocess.run(compiler_cmd)
+    if result.returncode != 0:
+        raise RuntimeError("Model compiler failed! Not connected to the internet?")
 
     return 0
