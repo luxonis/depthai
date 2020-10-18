c5e173b43ad444d72b846bb4be6b5044e4962828
saching13 <sachin.guruswamy@gmail.com>
2020-10-17 22:58:27 -0700

    added c and caps C along with value error for usb c check

--- depthai_demo.py
+++ depthai_demo.py
@@ -167,7 +167,7 @@ class DepthAI:
             }
             if key in keypress_handler_lut:
                 keypress_handler_lut[key]()
-            elif key == ord('c'):
+            elif key == ord('c') or key == ord('C'):
                 if 'jpegout' in stream_names:
                     self.device.request_jpeg()
                 else:
@@ -193,11 +193,11 @@ class DepthAI:
         if not self.device.is_usb3():
             fail_usb_img = cv2.imread(consts.resource_paths.usb_3_failed, cv2.IMREAD_COLOR)
             while True:
-                cv2.imshow('Calibration test Passed and wrote to EEPROM', fail_usb_img)
+                cv2.imshow('Connection over usb 3 failed', fail_usb_img)
                 k = cv2.waitKey(33)
-                if k == 32 or k == 27:  # Esc key to stop
-                    break
-                elif k == -1:  # normally -1 returned,so don't print it
+                if k != -1:  # Esc key to stop
+                    raise ValueError('Couldn\'t connect over USB3')
+                else:  # normally -1 returned,so don't print it
                     continue
 
         while self.runThread:
