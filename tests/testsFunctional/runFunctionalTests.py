e83a7851331c5052abf4a6aa10737cc47de977fc
Łukasz Piłatowski <lukpila29@gmail.com>
2020-10-27 11:57:47 +0100

    cleanup

--- tests/testsFunctional/runFunctionalTests.py
+++ tests/testsFunctional/runFunctionalTests.py
@@ -1,6 +1,7 @@
 import os
 import consts.resource_paths
 
+
 class TestManager:
     results = []
 
@@ -8,7 +9,6 @@ class TestManager:
         print("\033[1;34;40m----------------------------------------------------------------")
         print("Running " + testName)
         print("----------------------------------------------------------------\033[0m")
-
 
     def printResult(self, exitCode, testName):
         if exitCode == 0:
@@ -28,27 +28,25 @@ class TestManager:
             self.printResult(result[1], result[0])
 
 
-
 if __name__ == "__main__":
     testMan = TestManager()
     testPath = consts.resource_paths.tests_functional_path
 
     # try data streams
-    #meta_d2h
+    # meta_d2h
     result = os.system("python3 " + testPath + "test_meta_d2h.py -s " + "meta_d2h metaout")
     testMan.printResult(result, "meta_d2h")
     testMan.appendResult(["meta_d2h", result])
 
-    #jpegout - if we want to do a functional test here, we need some way to trigger and detect a jpeg capture from the test.
-    #result = os.system("python3 test_meta_d2h.py -s " + "previewout jpegout")
-    #testMan.printResult(result, "jpegout")
+    # jpegout - if we want to do a functional test here, we need some way to trigger and detect a jpeg capture from the test.
+    # result = os.system("python3 test_meta_d2h.py -s " + "previewout jpegout")
+    # testMan.printResult(result, "jpegout")
 
-    #object_tracker
+    # object_tracker
     result = os.system("python3 " + testPath + "test_object_tracker.py -s " + "metaout object_tracker previewout")
     testMan.printResult(result, "object_tracker")
     print(result)
     testMan.appendResult(["object_tracker", result])
-
 
     # try visual streams.
     visualStreams = [
@@ -66,19 +64,16 @@ if __name__ == "__main__":
         testMan.printResult(result, stream)
         testMan.appendResult([stream, result])
 
-
-    #try various -cnn options
+    # try various -cnn options
     cnndir = consts.resource_paths.nn_resource_path
     cnnlist = os.listdir(cnndir)
     for cnn in cnnlist:
-        print(cnndir+cnn)
-        if os.path.isdir(cnndir+cnn):
+        print(cnndir + cnn)
+        if os.path.isdir(cnndir + cnn):
             testMan.printTestName(cnn)
             runStr = "python3 " + testPath + "test_video.py -cnn " + cnn
             result = os.system(runStr)
             testMan.printResult(result, cnn)
             testMan.appendResult([cnn, result])
 
-
     testMan.printSummary()
-
