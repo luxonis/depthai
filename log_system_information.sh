5234b3e842aeff493ed557c35b0b3eede17696f1
Łukasz Piłatowski <lukpila29@gmail.com>
2020-10-27 16:23:20 +0100

    Update log_system_information.sh
    Former-commit-id: 8f1a7a9302ebaa29724d08165a85c8c27a89ec0e

--- log_system_information.sh
+++ log_system_information.sh
@@ -1,4 +1,46 @@
-logSystemInformation="log_system_information.txt"
-uname -a | tee -a $logSystemInformation
-python3 -c "import depthai; import numpy; print('depthai.__version__ == %s' % depthai.__version__); print('depthai.__dev_version__ == %s' % depthai.__dev_version__); print('numpy.__version__ == %s' % numpy.__version__);" | tee -a $logSystemInformation
-python3 --version | tee -a $logSystemInformation
+{
+  if [[ $(pwd) != *"depthai" ]]; then
+    echo "You need to run this script from the cloned depthai repository (you can clone it from here - https://github.com/luxonis/depthai)"
+    exit 1
+  fi
+
+  echo "Installing required tools for the debug script..."
+  sudo apt-get install util-linux usbutils lshw coreutils
+
+  echo "Collecting HW specs..."
+  echo "[lscpu]"
+  sudo lscpu
+  echo "[lsusb]"
+  sudo lsusb
+  echo "[uname -a]"
+  sudo uname -a
+  echo "[lshw -short]"
+  sudo lshw -short
+  echo "[lsblk]"
+  sudo lsblk
+
+  echo
+  echo "Collecting installed software..."
+  echo "[apt list --installed]"
+  sudo apt list --installed
+  echo "[pip freeze]"
+  python3 -m pip freeze
+
+  echo
+  echo "Checking the environment..."
+  echo "[printenv]"
+  printenv
+
+  echo
+  echo "Checking the versions..."
+  echo "[python3 --version]"
+  python3 --version
+  echo "[python3 -m pip --version]"
+  python3 -m pip --version
+  echo "[git log -n 1]"
+  git log -n 1
+  echo "[git branch]"
+  git branch
+  echo "[git submodule status]"
+  git submodule status
+} > log_system_information.txt
