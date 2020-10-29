0e242976937e704c7596bee7f1828709749a89e1
Łukasz Piłatowski <lukpila29@gmail.com>
2020-10-29 12:14:44 +0100

    rename runner file
    Former-commit-id: 1e2c0a62d42c1a6356c0c8edd37a7a0bdc33fdb8

--- run_tests.py
+++ run_tests.py
@@ -4,5 +4,5 @@ from pathlib import Path
 
 env = os.environ.copy()
 env["PYTHONPATH"] = str(Path(__file__).parent)
-result = subprocess.run(["python3", "./tests/run_tests.py"], env=env)
+result = subprocess.run(["python3", "./tests/tests_runner.py"], env=env)
 exit(result.returncode)
