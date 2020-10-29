0a1849d690364b941e831f92680462c6476c64a3
Łukasz Piłatowski <lukpila29@gmail.com>
2020-10-29 16:05:48 +0100

    use sys.executable instead of python3
    Former-commit-id: 142656513d9fb8d478b4916a59f4b5babda8aaf1

--- run_tests.py
+++ run_tests.py
@@ -3,8 +3,9 @@
 import subprocess
 import os
 from pathlib import Path
+import sys
 
 env = os.environ.copy()
 env["PYTHONPATH"] = str(Path(__file__).parent)
-result = subprocess.run(["python3", "./tests/tests_runner.py"], env=env)
+result = subprocess.run([sys.executable, "./tests/tests_runner.py"], env=env)
 exit(result.returncode)
