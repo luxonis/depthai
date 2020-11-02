a71d48ae7f7d5f4349d6c48784b613a209b8ba70
Łukasz Piłatowski <lukpila29@gmail.com>
2020-11-02 13:54:40 +0100

    Add reruns to handle model downloader test timeout

--- run_tests.py
+++ run_tests.py
@@ -8,5 +8,5 @@ import sys
 env = os.environ.copy()
 curr_dir = Path(__file__).parent
 env["PYTHONPATH"] = str(curr_dir)
-result = subprocess.run([sys.executable, curr_dir / Path("./tests/tests_runner.py")], env=env)
+result = subprocess.run([sys.executable, curr_dir / Path("./tests/tests_runner.py"), "--reruns", "1"], env=env)
 exit(result.returncode)
