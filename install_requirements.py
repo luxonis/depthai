d5b7ec5e2e93ecc400c09a4c96bc5caf0bd8f30a
Łukasz Piłatowski <lukpila29@gmail.com>
2020-12-07 11:31:12 +0100

    move --user after the directive

--- install_requirements.py
+++ install_requirements.py
@@ -6,11 +6,14 @@ import sys
 # https://stackoverflow.com/a/58026969/5494277
 in_venv = getattr(sys, "real_prefix", getattr(sys, "base_prefix", sys.prefix)) != sys.prefix
 pip_call = [sys.executable, "-m", "pip"]
+pip_install = pip_call + ["install"]
+pip_uninstall = pip_call + ["uninstall"]
 
 if not in_venv:
-    pip_call.append("--user")
+    pip_install.append("--user")
+    pip_uninstall.append("--user")
 
-subprocess.check_call([*pip_call, "install", "pip", "-U"])
+subprocess.check_call([*pip_install, "pip", "-U"])
 # temporary workaroud for issue between main and develop
-subprocess.check_call([*pip_call, "uninstall", "depthai", "--yes"])
-subprocess.check_call([*pip_call, "install", "-r", "requirements.txt"])
+subprocess.check_call([*pip_uninstall, "depthai", "--yes"])
+subprocess.check_call([*pip_install, "-r", "requirements.txt"])
