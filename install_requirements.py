294086cdc7bf2267297deb32c020277bc7b73b88
Łukasz Piłatowski <lukpila29@gmail.com>
2020-12-07 11:32:04 +0100

    remove --user from uninstall

--- install_requirements.py
+++ install_requirements.py
@@ -7,13 +7,11 @@ import sys
 in_venv = getattr(sys, "real_prefix", getattr(sys, "base_prefix", sys.prefix)) != sys.prefix
 pip_call = [sys.executable, "-m", "pip"]
 pip_install = pip_call + ["install"]
-pip_uninstall = pip_call + ["uninstall"]
 
 if not in_venv:
     pip_install.append("--user")
-    pip_uninstall.append("--user")
 
 subprocess.check_call([*pip_install, "pip", "-U"])
 # temporary workaroud for issue between main and develop
-subprocess.check_call([*pip_uninstall, "depthai", "--yes"])
+subprocess.check_call([*pip_call, "uninstall", "depthai", "--yes"])
 subprocess.check_call([*pip_install, "-r", "requirements.txt"])
