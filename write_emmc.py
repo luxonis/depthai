6c519739b836658ea0873a8aed4fd5eb0dd8ffc7
saching13 <sachin.guruswamy@gmail.com>
2021-01-25 19:01:25 -0800

    updated write emmc

--- write_emmc.py
+++ write_emmc.py
@@ -6,7 +6,8 @@ import re
 import subprocess
 from time import sleep, time
 import argparse
-
+from pathlib import Path
+import sys
 # device_re = re.compile("Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
 # df = subprocess.check_output("lsusb")
 # devices = []
@@ -19,10 +20,33 @@ import argparse
 #             devices.append(dinfo)
 # print(devices)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-img", help="place the img file in the same directory as write_emmc.py and pass the filename here",
+                    type=str)
+
+
+curr_path = Path(__file__).parent.resolve()
+args = parser.parse_args()
+img_path = os.path.join(curr_path, args.img)
+print(img_path)
+img_file = Path(img_path)
+if not img_file.exists():
+    print("file does not exist. please input right image file")
+    sys.exit(1)
+
+home = str(Path.home())
+boot_pth = home + "/usbboot/rpiboot"
+boot_file = Path(boot_pth)
+if not boot_file.exists():
+    print("Couldn't fild rpi boot file at {}".format(boot_pth))
+    sys.exit(1)
+
+
 
 # install rpiboot first
 def start_rpi_boot():
-    response = os.system("sudo ./rpiboot ")
+    boot_cmd = "sudo " + boot_pth
+    response = os.system(boot_cmd)
     # and then check the response...
     if response == 0:
         eMMC_status = True
@@ -31,8 +55,16 @@ def start_rpi_boot():
 
     return eMMC_status
 
+print("Set jumper to enable on the board.")
+print("Connect the device to a linux-pc using usb cable.")
+print("Press \"enter\" when ready to start searching for the device")
+print("After pressing \"enter\" connect the device to the power source")
 
-# start_rpi_boot()
+input()
+start_rpi_boot()
+print("Search Complete....!")
+
+sleep(2)
 
 myblkd = BlkDiskInfo()
 filters = {
@@ -41,18 +73,28 @@ filters = {
 all_my_disks = myblkd.get_disks(filters)
 json_output = json.dumps(all_my_disks)
 print(json_output)
+divident = 10**9
+
+print("Image will be written to the drives shown below.")
+for device_obj in all_my_disks:
+    disk_size = int(device_obj["size"]) / divident
+    print(disk_size)
+    if disk_size < 33 and disk_size > 14: 
+        print("Device name: {0}  size:{1}".format(device_obj["name"], disk_size))
+
+print("Press Enter key to continue writing the OS. [ctrl + c to abort]")
+input()
 
 response_status = []
 start = time()
 
-divident = 10**9
 
 for item in all_my_disks:
     # print(int(item["size"]))
     disk_size = int(item["size"]) / divident
     print(disk_size)
-    if disk_size < 18 and disk_size > 14: 
-        test_cmd = "sudo dd if=/home/sachin/PiShrink/conv_rpi.img of=/dev/" + item["name"] + " bs=4M conv=fsync status=progress"
+    if disk_size < 33 and disk_size > 14: 
+        test_cmd = "sudo dd if=" + img_path + " of=/dev/" + item["name"] + " bs=4M conv=fsync status=progress"
         print(test_cmd)
         p = subprocess.Popen(test_cmd, shell=True, preexec_fn=os.setsid)
         response_status.append(p)
@@ -63,8 +105,7 @@ while not is_finished:
     for proc in response_status:
         if proc.poll() is None:
             temp_status = False
-    sleep(1)
-    print("waiting..")
+    
     is_finished = temp_status
 
 end = time()
