34c7a4a6d48e11080df594a033c9ddc53d11f220
Łukasz Piłatowski <lpilatowski@teonite.com>
2021-06-21 12:01:29 +0200

    rename --force_usb2 to --usb_speed usb2

--- depthai_helpers/arg_manager.py
+++ depthai_helpers/arg_manager.py
@@ -84,5 +84,5 @@ def parse_args():
                         help="Mono cam fps: max 60.0 for H:720 or H:800, max 120.0 for H:400. Default: %(default)s")
     parser.add_argument('-cb', '--callback', type=Path, default=project_root / 'callbacks.py', help="Path to callbacks file to be used. Default: %(default)s")
     parser.add_argument("--openvino_version", type=str, choices=openvino_versions, help="Specify which OpenVINO version to use in the pipeline")
-    parser.add_argument('-fusb2', '--force_usb2', action="store_true", help="Force USB2 communication mode.")
+    parser.add_argument('-usbs', '--usb_speed', type=str, default="usb3", choices=["usb2", "usb3"], help="Force USB communication speed. Default: %(default)s")
     return parser.parse_args()
