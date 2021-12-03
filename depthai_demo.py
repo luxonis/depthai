9494dae1d6c5c198c30965c23a34d7e00b229258
Łukasz Piłatowski <lukpila29@gmail.com>
2021-12-03 20:20:39 +0100

    add libraries path and downgrade contrib version

--- depthai_demo.py
+++ depthai_demo.py
@@ -496,6 +496,7 @@ def runQt():
     from gui.main import DemoQtGui
     from PyQt5.QtWidgets import QMessageBox
     from PyQt5.QtCore import QObject, pyqtSignal, QRunnable, QThreadPool
+    os.environ["LD_LIBRARY_PATH"] = QLibraryInfo.location(QLibraryInfo.LibrariesPath)
     os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = QLibraryInfo.location(QLibraryInfo.PluginsPath)
     os.environ["QT_QUICK_BACKEND"] = "software"
 
