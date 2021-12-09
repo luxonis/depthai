8e3d791b1fcc5a87f0afe9a5a546799eeac26758
Łukasz Piłatowski <lukpila29@gmail.com>
2021-12-09 14:41:44 +0100

    change supervised run logic

--- depthai_helpers/supervisor.py
+++ depthai_helpers/supervisor.py
@@ -20,25 +20,26 @@ def createNewArgs(args):
 
 class Supervisor:
     def runDemo(self, args):
-        new_env = os.environ.copy()
+        args.noSupervisor = True
+        new_args = createNewArgs(args)
+
         if args.guiType == "qt":
+            new_env = os.environ.copy()
             from PyQt5.QtCore import QLibraryInfo
             new_env["QT_QPA_PLATFORM_PLUGIN_PATH"] = QLibraryInfo.location(QLibraryInfo.PluginsPath)
             new_env["QT_QUICK_BACKEND"] = "software"
             new_env["LD_LIBRARY_PATH"] = QLibraryInfo.location(QLibraryInfo.LibrariesPath)
-        new_env["DEPTHAI_INSTALL_SIGNAL_HANDLER"] = "0"
-        args.noSupervisor = True
-
-        try:
-            new_args = createNewArgs(args)
-            subprocess.check_call(sys.argv[:2] + new_args, env=new_env)
-        except subprocess.CalledProcessError as ex:
-            if args.guiType != "qt":
-                raise
-            print("Error while running demo script... {}".format(ex))
-            print("Waiting 5s for the device to be discoverable again...")
-            time.sleep(5)
-            args.guiType = "cv"
+            new_env["DEPTHAI_INSTALL_SIGNAL_HANDLER"] = "0"
+            try:
+                subprocess.check_call(sys.argv[:2] + new_args, env=new_env)
+            except subprocess.CalledProcessError as ex:
+                print("Error while running demo script... {}".format(ex))
+                print("Waiting 5s for the device to be discoverable again...")
+                time.sleep(5)
+                args.guiType = "cv"
+        if args.guiType == "cv":
+            new_env = os.environ.copy()
+            new_env["DEPTHAI_INSTALL_SIGNAL_HANDLER"] = "0"
             new_args = createNewArgs(args)
             subprocess.check_call(sys.argv[:2] + new_args, env=new_env)
 
