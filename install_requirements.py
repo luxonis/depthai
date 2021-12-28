50461be044144020d287ccfb785f9580a719fd5e
Łukasz Piłatowski <lukpila29@gmail.com>
2021-12-28 14:27:31 +0100

    remove warning message

--- install_requirements.py
+++ install_requirements.py
@@ -50,25 +50,3 @@ try:
     subprocess.check_call(pip_package_install + ["-r", "requirements-optional.txt"], cwd=scriptDirectory, stderr=subprocess.DEVNULL)
 except subprocess.CalledProcessError as ex:
     print("Optional dependencies were not installed. This is not an error.")
-
-
-if thisPlatform == "aarch64":
-    # try to import opencv, numpy in a subprocess, since it might fail with illegal instruction
-    # if it was previously installed w/ pip without setting OPENBLAS_CORE_TYPE=ARMV8 env variable
-    opencvInstalledProperly = False
-    try:
-        subprocess.check_call([sys.executable, "-c", "import numpy, cv2;"])
-        opencvInstalledProperly = True
-    except subprocess.CalledProcessError as ex:
-        opencvInstalledProperly = False
-
-    if not opencvInstalledProperly:
-        from os import environ
-        OPENBLAS_CORE_TYPE = environ.get('OPENBLAS_CORE_TYPE')
-        if OPENBLAS_CORE_TYPE != 'ARMV8':
-            WARNING='\033[1;5;31m'
-            RED='\033[91m'
-            LINE_CL='\033[0m'
-            SUGGESTION='echo "export OPENBLAS_CORETYPE=ARMV8" >> ~/.bashrc && source ~/.bashrc'
-            print(f'{WARNING}WARNING:{LINE_CL} Need to set OPENBLAS_CORE_TYPE environment variable, otherwise opencv will fail with illegal instruction.')
-            print(f'Run: {RED}{SUGGESTION}{LINE_CL}')
