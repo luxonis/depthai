4786d7b03f143e24323d4eb3adc52f76268b6d6f
Sachin <sachin.guruswamy@gmail.com>
2022-01-13 11:02:45 -0800

    WIP nmap

--- nmap_rpi.py
+++ nmap_rpi.py
@@ -2,10 +2,13 @@
 import nmap
 nmScan = nmap.PortScanner()
 res = nmScan.scan(hosts='192.168.0.0/24', arguments='-sn')
-print(res['scan'])
-
+# print(res['scan'])
+for key in res['scan'].keys():
+    print(res['scan'][key])
+    print('----------------------------------------')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 res = nmScan.scan(hosts='192.168.0.0/24', arguments='-sP')
-print(res['scan'])
+# print(res['scan'])
 for key in res['scan'].keys():
     print(res['scan'][key])
     print('----------------------------------------')
