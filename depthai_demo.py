4379c0677331cda01a871a95a50e99652be531e8
Łukasz Piłatowski <lukpila29@gmail.com>
2022-02-12 20:51:49 +0100

    Fix streams to show resolve method

--- depthai_demo.py
+++ depthai_demo.py
@@ -615,15 +615,19 @@ def runQt():
                     self.conf.args.deviceId = defaultDevice
             if Previews.color.name not in self.conf.args.show:
                 self.conf.args.show.append(Previews.color.name)
-            if Previews.nnInput.name not in self.conf.args.show:
+            if self.conf.useNN and Previews.nnInput.name not in self.conf.args.show:
                 self.conf.args.show.append(Previews.nnInput.name)
-            if Previews.depth.name not in self.conf.args.show and Previews.disparityColor.name not in self.conf.args.show:
+            if self.conf.useDepth and not self.parent.useDisparity and Previews.depth.name not in self.conf.args.show:
                 self.conf.args.show.append(Previews.depth.name)
-            if Previews.depthRaw.name not in self.conf.args.show and Previews.disparity.name not in self.conf.args.show:
+            if self.conf.useDepth and not self.parent.useDisparity and Previews.depthRaw.name not in self.conf.args.show:
                 self.conf.args.show.append(Previews.depthRaw.name)
+            if self.conf.useDepth and self.parent.useDisparity and Previews.disparity.name not in self.conf.args.show:
+                self.conf.args.show.append(Previews.disparity.name)
+            if self.conf.useDepth and self.parent.useDisparity and Previews.disparityColor.name not in self.conf.args.show:
+                self.conf.args.show.append(Previews.disparityColor.name)
             if Previews.left.name not in self.conf.args.show:
                 self.conf.args.show.append(Previews.left.name)
-            if Previews.rectifiedLeft.name not in self.conf.args.show:
+            if self.conf.useDepth and Previews.rectifiedLeft.name not in self.conf.args.show:
                 self.conf.args.show.append(Previews.rectifiedLeft.name)
             if Previews.right.name not in self.conf.args.show:
                 self.conf.args.show.append(Previews.right.name)
