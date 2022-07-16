62189e7464ebd6eacf26a0eb31441cefc483ca86
Erol444 <erol123444@gmail.com>
2022-07-17 01:56:37 +0200

    Updated ps1 script

--- launcher/windows/src/create_shortcut.ps1
+++ launcher/windows/src/create_shortcut.ps1
@@ -1,9 +1,17 @@
 # Create a DepthAI.lnk with icon
+$TargetPath = "$PSScriptRoot\venv\Scripts\python.exe"
+$Arguments = "`"$PSScriptRoot\depthai\launcher\launcher.py`" --repo `"$PSScriptRoot\depthai`" --git `"$PSScriptRoot\PortableGit\bin\git.exe`""
+$BatFile = "$PSScriptRoot\run.bat"
+
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$PSScriptRoot\DepthAI.lnk")
-$Shortcut.TargetPath = "$PSScriptRoot\venv\Scripts\python.exe"
-$Shortcut.Arguments = "`"$PSScriptRoot\depthai\launcher\launcher.py`" --repo `"$PSScriptRoot\depthai`" --git `"$PSScriptRoot\PortableGit\bin\git.exe`""
+$Shortcut.TargetPath = "cmd"
+$Shortcut.Arguments = "/c $BatFile"
 $Shortcut.IconLocation = "$PSScriptRoot\logo_only_EBl_icon.ico"
 $Shortcut.WorkingDirectory = "$PSScriptRoot"
 $Shortcut.WindowStyle = 7 # Minimized
 $Shortcut.Save()
+
+
+$StartCommand = "$TargetPath $Arguments"
+Set-Content -Path $BatFile -Value $StartCommand -Encoding ASCII
