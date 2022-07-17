bd9693c2460a53a914ee492f14e86aeac37b9a29
Erol444 <erol123444@gmail.com>
2022-07-17 14:52:53 +0200

    Updated to use power shell  script

--- launcher/windows/src/create_shortcut.ps1
+++ launcher/windows/src/create_shortcut.ps1
@@ -1,17 +1,17 @@
 # Create a DepthAI.lnk with icon
-$TargetPath = "$PSScriptRoot\venv\Scripts\python.exe"
+$Command = "$PSScriptRoot\venv\Scripts\python.exe"
 $Arguments = "`"$PSScriptRoot\depthai\launcher\launcher.py`" --repo `"$PSScriptRoot\depthai`" --git `"$PSScriptRoot\PortableGit\bin\git.exe`""
-$BatFile = "$PSScriptRoot\run.bat"
+$Ps1File = "$PSScriptRoot\run.ps1"
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$PSScriptRoot\DepthAI.lnk")
-$Shortcut.TargetPath = "cmd"
-$Shortcut.Arguments = "/c $BatFile"
+$Shortcut.TargetPath = "pwsh"
+$Shortcut.Arguments = "-noexit -ExecutionPolicy Bypass -File " + $Ps1File
 $Shortcut.IconLocation = "$PSScriptRoot\logo_only_EBl_icon.ico"
 $Shortcut.WorkingDirectory = "$PSScriptRoot"
 $Shortcut.WindowStyle = 7 # Minimized
 $Shortcut.Save()
 
 
-$StartCommand = "$TargetPath $Arguments"
-Set-Content -Path $BatFile -Value $StartCommand -Encoding ASCII
+$StartCommand = "$Command $Arguments"
+Set-Content -Path $Ps1File -Value $StartCommand -Encoding ASCII
