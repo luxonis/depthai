fa35e15f1b32dd17991b109761d3da9c604a7890
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-10-04 00:01:50 +0200

    Add Out class to IMUComponent

--- depthai_sdk/src/depthai_sdk/components/imu_component.py
+++ depthai_sdk/src/depthai_sdk/components/imu_component.py
@@ -1,14 +1,16 @@
 import depthai as dai
 
 from .component import Component, XoutBase
-from ..oak_outputs.xout_base import StreamXout
 from ..oak_outputs.xout import XoutIMU
+from ..oak_outputs.xout_base import StreamXout
 
 
 class IMUComponent(Component):
     node: dai.node.IMU
 
     def __init__(self, pipeline: dai.Pipeline):
+        self.out = self.Out(self)
+
         super().__init__()
         self.node = pipeline.createIMU()
         self.config_imu()
@@ -38,11 +40,25 @@ class IMUComponent(Component):
         self.node.setMaxBatchReports(maxBatchReports=max_batch_reports)
         self.node.enableFirmwareUpdate(enable_firmware_update)
 
-    def out(self, pipeline: dai.Pipeline, device: dai.Device) -> XoutBase:
-        out = self.node.out
-        out = StreamXout(self.node.id, out)
-        imu_out = XoutIMU(out)
-        return super()._create_xout(pipeline, imu_out)
-
     def _update_device_info(self, pipeline: dai.Pipeline, device: dai.Device, version: dai.OpenVINO.Version):
         pass
+
+    class Out:
+        _comp: 'IMUComponent'
+
+        def __init__(self, imuComponent: 'IMUComponent'):
+            self._comp = imuComponent
+
+        def main(self, pipeline: dai.Pipeline, device: dai.Device) -> XoutBase:
+            """
+            Default output. Uses either camera(), replay(), or encoded() depending on the component settings.
+            """
+            return self.text(pipeline, device)
+
+        def text(self, pipeline: dai.Pipeline, device: dai.Device) -> XoutBase:
+            out = self._comp.node.out
+            out = StreamXout(self._comp.node.id, out)
+            imu_out = XoutIMU(out)
+            return self._comp._create_xout(pipeline, imu_out)
+
+    out: Out
