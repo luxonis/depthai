e0e96a038799a03ec9804b74adf169b83ea73b7d
Erol444 <erol123444@gmail.com>
2022-10-03 23:18:47 +0200

    Fixed mobilenet example

--- depthai_sdk/examples/mobilenet.py
+++ depthai_sdk/examples/mobilenet.py
@@ -5,7 +5,7 @@ from depthai_sdk import OakCamera
 with OakCamera() as oak:
     color = oak.create_camera('color')
     nn = oak.create_nn('mobilenet-ssd', color, spatial=True)
-    oak.visualize([nn.out, nn.out_passthrough])
-    oak.visualize(nn.out_spatials, scale=1/2)
+    oak.visualize([nn.out.main, nn.out.passthrough])
+    oak.visualize(nn.out.spatials, scale=1/2)
     # oak.show_graph()
     oak.start(blocking=True)
