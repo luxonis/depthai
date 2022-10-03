04100f42933915b711a153ab3c187155ae72223b
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-10-04 00:02:08 +0200

    Update imu.py example

--- depthai_sdk/examples/imu.py
+++ depthai_sdk/examples/imu.py
@@ -1,17 +1,15 @@
-from depthai_sdk import OakCamera
-
 from depthai_sdk.classes.packets import IMUPacket
 
-with OakCamera() as oak:
-    camera = oak.create_camera('color', fps=30)
+from depthai_sdk import OakCamera
 
+
+def callback(packet: IMUPacket):
+    print(packet)
+
+
+with OakCamera() as oak:
     imu = oak.create_imu()
     imu.config_imu(report_rate=400, batch_report_threshold=5)
 
-
-    def cb(packet: IMUPacket):
-        print(packet)
-
-
-    oak.callback(imu, callback=cb)
+    oak.callback(imu.out.main, callback=callback)
     oak.start(blocking=True)
