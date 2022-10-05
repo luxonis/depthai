b90dbfc830705fd6ded085a6aa7774862badc974
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-10-05 20:14:54 +0200

    Add different visualization styles to examples/visualizer.py

--- depthai_sdk/examples/visualizer.py
+++ depthai_sdk/examples/visualizer.py
@@ -1,23 +1,45 @@
-from depthai_sdk import NewVisualizer
-from depthai_sdk import OakCamera, DetectionPacket
+from depthai_sdk.visualize.configs import BboxStyle
 
+from depthai_sdk import OakCamera
 
 with OakCamera() as oak:
     camera = oak.create_camera('color')
-
     det = oak.create_nn('face-detection-retail-0004', camera)
 
     visualizer = oak.visualize(det.out.main)
     visualizer.configure_bbox(
         color=(0, 255, 0),
-        box_roundness=0.5,
-        thickness=2
+        thickness=2,
+        bbox_style=BboxStyle.RECTANGLE,
+    ).configure_text(
+        font_color=(0, 255, 0)
     )
 
     visualizer2 = oak.visualize(det.out.main)
     visualizer2.configure_bbox(
+        fill_transparency=0.7,
+        thickness=3,
+        bbox_style=BboxStyle.CORNERS,
+        hide_label=True
+    )
+
+    visualizer3 = oak.visualize(det.out.main)
+    visualizer3.configure_bbox(
         fill_transparency=0.5,
-        thickness=-1
+        box_roundness=25,
+        thickness=1,
+        bbox_style=BboxStyle.ROUNDED_RECTANGLE,
+        hide_label=True
+    )
+
+    visualizer4 = oak.visualize(det.out.main)
+    visualizer4.configure_bbox(
+        fill_transparency=0,
+        box_roundness=15,
+        thickness=1,
+        bbox_style=BboxStyle.ROUNDED_CORNERS,
+    ).configure_text(
+        font_color=(0, 255, 255)
     )
 
     oak.start(blocking=True)
