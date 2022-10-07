4169c7d6e3e9feaf4003f4a34069bcd7c3fa8bb9
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-10-07 18:19:28 +0200

    Add example showing how tracking on video works

---
+++ depthai_sdk/examples/car_tracking.py
@@ -0,0 +1,18 @@
+from depthai import UsbSpeed
+
+from depthai_sdk import OakCamera, AspectRatioResizeMode
+
+with OakCamera(recording='cars-tracking-above-01') as oak:
+    color = oak.create_camera('color')
+    nn = oak.create_nn('vehicle-detection-0202', color, tracker=True)
+    nn.config_nn(aspectRatioResizeMode=AspectRatioResizeMode.STRETCH)
+
+    visualizer = oak.visualize(nn.out.tracker)
+    visualizer.configure_output(
+        show_fps=True,
+        img_scale=1.0,
+    ).configure_tracking(
+        line_thickness=5
+    )
+
+    oak.start(blocking=True)
