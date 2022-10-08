e959a06b96a7ebb7737dd560c399222982b869bd
Daniel Calaver <calaver@luxonis.com>
2022-10-08 22:13:00 +0300

    adapt calib script to main depthai

--- calibrate.py
+++ calibrate.py
@@ -23,57 +23,71 @@ debug = False
 red = (255, 0, 0)
 green = (0, 255, 0)
 
-stringToCam = {
-                # 'RGB': dai.CameraBoardSocket.RGB,
-                # 'LEFT': dai.CameraBoardSocket.LEFT,
-                # 'RIGHT': dai.CameraBoardSocket.RIGHT,
-                # 'AUTO': dai.CameraBoardSocket.AUTO,
-                # 'CAM_A' : dai.CameraBoardSocket.RGB,
-                # 'CAM_B' : dai.CameraBoardSocket.LEFT,
-                # 'CAM_C' : dai.CameraBoardSocket.CAM_C,
-                'RGB'   : dai.CameraBoardSocket.CAM_A,
-                'LEFT'  : dai.CameraBoardSocket.CAM_B,
-                'RIGHT' : dai.CameraBoardSocket.CAM_C,
-                'CAM_A' : dai.CameraBoardSocket.CAM_A,
-                'CAM_B' : dai.CameraBoardSocket.CAM_B,
-                'CAM_C' : dai.CameraBoardSocket.CAM_C,
-                'CAM_D' : dai.CameraBoardSocket.CAM_D,
-                'CAM_E' : dai.CameraBoardSocket.CAM_E,
-                'CAM_F' : dai.CameraBoardSocket.CAM_F,
-                'CAM_G' : dai.CameraBoardSocket.CAM_G,
-                'CAM_H' : dai.CameraBoardSocket.CAM_H
-                }
+if hasattr(dai.CameraBoardSocket, 'CAM_A'):
+    stringToCam = {
+                    'RGB'   : dai.CameraBoardSocket.CAM_A,
+                    'LEFT'  : dai.CameraBoardSocket.CAM_B,
+                    'RIGHT' : dai.CameraBoardSocket.CAM_C,
+                    'CAM_A' : dai.CameraBoardSocket.CAM_A,
+                    'CAM_B' : dai.CameraBoardSocket.CAM_B,
+                    'CAM_C' : dai.CameraBoardSocket.CAM_C,
+                    'CAM_D' : dai.CameraBoardSocket.CAM_D,
+                    'CAM_E' : dai.CameraBoardSocket.CAM_E,
+                    'CAM_F' : dai.CameraBoardSocket.CAM_F,
+                    'CAM_G' : dai.CameraBoardSocket.CAM_G,
+                    'CAM_H' : dai.CameraBoardSocket.CAM_H
+                    }
+    camToString = {
+                    dai.CameraBoardSocket.CAM_A : 'RGB'  ,
+                    dai.CameraBoardSocket.CAM_B : 'LEFT' ,
+                    dai.CameraBoardSocket.CAM_C : 'RIGHT',
+                    dai.CameraBoardSocket.CAM_A : 'CAM_A',
+                    dai.CameraBoardSocket.CAM_B : 'CAM_B',
+                    dai.CameraBoardSocket.CAM_C : 'CAM_C',
+                    dai.CameraBoardSocket.CAM_D : 'CAM_D',
+                    dai.CameraBoardSocket.CAM_E : 'CAM_E',
+                    dai.CameraBoardSocket.CAM_F : 'CAM_F',
+                    dai.CameraBoardSocket.CAM_G : 'CAM_G',
+                    dai.CameraBoardSocket.CAM_H : 'CAM_H'
+                    }
+else:
+    stringToCam = {
+                    'RGB': dai.CameraBoardSocket.RGB,
+                    'LEFT': dai.CameraBoardSocket.LEFT,
+                    'RIGHT': dai.CameraBoardSocket.RIGHT,
+                    'AUTO': dai.CameraBoardSocket.AUTO,
+                    'CAM_A' : dai.CameraBoardSocket.RGB,
+                    'CAM_B' : dai.CameraBoardSocket.LEFT,
+                    'CAM_C' : dai.CameraBoardSocket.RIGHT
+                   }
 
-CamToString = {
-                # dai.CameraBoardSocket.RGB : 'RGB'  ,
-                # dai.CameraBoardSocket.LEFT : 'LEFT' ,
-                # dai.CameraBoardSocket.RIGHT : 'RIGHT',
-                # dai.CameraBoardSocket.AUTO : 'AUTO'
-                dai.CameraBoardSocket.CAM_A : 'RGB'  ,
-                dai.CameraBoardSocket.CAM_B : 'LEFT' ,
-                dai.CameraBoardSocket.CAM_C : 'RIGHT',
-                dai.CameraBoardSocket.CAM_A : 'CAM_A',
-                dai.CameraBoardSocket.CAM_B : 'CAM_B',
-                dai.CameraBoardSocket.CAM_C : 'CAM_C',
-                dai.CameraBoardSocket.CAM_D : 'CAM_D',
-                dai.CameraBoardSocket.CAM_E : 'CAM_E',
-                dai.CameraBoardSocket.CAM_F : 'CAM_F',
-                dai.CameraBoardSocket.CAM_G : 'CAM_G',
-                dai.CameraBoardSocket.CAM_H : 'CAM_H'
-                }
+    camToString = {
+                    # dai.CameraBoardSocket.RGB : 'RGB'  ,
+                    # dai.CameraBoardSocket.LEFT : 'LEFT' ,
+                    # dai.CameraBoardSocket.RIGHT : 'RIGHT',
+                    # dai.CameraBoardSocket.AUTO : 'AUTO',
+                    dai.CameraBoardSocket.RGB : 'CAM_A',
+                    dai.CameraBoardSocket.LEFT : 'CAM_B',
+                    dai.CameraBoardSocket.RIGHT : 'CAM_C',
+                    }
+
 
 camToMonoRes = {
                 'OV7251' : dai.MonoCameraProperties.SensorResolution.THE_480_P,
                 'OV9*82' : dai.MonoCameraProperties.SensorResolution.THE_800_P,
+                'OV9282' : dai.MonoCameraProperties.SensorResolution.THE_800_P,
                 }
 
 camToRgbRes = {
                 'IMX378' : dai.ColorCameraProperties.SensorResolution.THE_4_K,
                 'IMX214' : dai.ColorCameraProperties.SensorResolution.THE_4_K,
                 'OV9*82' : dai.ColorCameraProperties.SensorResolution.THE_800_P,
+                'OV9282' : dai.ColorCameraProperties.SensorResolution.THE_800_P,
                 'IMX582' : dai.ColorCameraProperties.SensorResolution.THE_12_MP,
-                'AR0234' : dai.ColorCameraProperties.SensorResolution.THE_1200_P
                 }
+
+if hasattr(dai.ColorCameraProperties.SensorResolution, 'THE_1200_P'):
+    camToRgbRes['AR0234'] = dai.ColorCameraProperties.SensorResolution.THE_1200_P
 
 def create_blank(width, height, rgb_color=(0, 0, 0)):
     """Create new image(numpy array) filled with certain color in RGB"""
@@ -308,16 +322,21 @@ class Main:
         #     "OAK-D-Lite Calibration is not supported on main yet. Please use `lite_calibration` branch to calibrate your OAK-D-Lite!!")
         
         self.device = dai.Device()
-        cameraProperties = self.device.getConnectedCameraProperties()
-        for properties in cameraProperties:
-            for in_cam in self.board_config['cameras'].keys():
-                cam_info = self.board_config['cameras'][in_cam]
-                if properties.socket == stringToCam[in_cam]:
-                    self.board_config['cameras'][in_cam]['sensorName'] = properties.sensorName
-                    print('Cam: {} and focus: {}'.format(cam_info['name'], properties.hasAutofocus))
-                    self.board_config['cameras'][in_cam]['hasAutofocus'] = properties.hasAutofocus
-                    # self.auto_checkbox_dict[cam_info['name']  + '-Camera-connected'].check()
-                    break
+        if hasattr(self.device, 'getConnectedCameraProperties'):
+            cameraProperties = self.device.getConnectedCameraProperties()
+            for properties in cameraProperties:
+                for in_cam in self.board_config['cameras'].keys():
+                    cam_info = self.board_config['cameras'][in_cam]
+                    if properties.socket == stringToCam[in_cam]:
+                        self.board_config['cameras'][in_cam]['sensorName'] = properties.sensorName
+                        print('Cam: {} and focus: {}'.format(cam_info['name'], properties.hasAutofocus))
+                        self.board_config['cameras'][in_cam]['hasAutofocus'] = properties.hasAutofocus
+                        # self.auto_checkbox_dict[cam_info['name']  + '-Camera-connected'].check()
+                        break
+        else:
+            sensors = self.device.getCameraSensorNames()
+            for sensor in sensors:
+                self.board_config['cameras'][camToString[sensor]]['sensorName'] = sensors[sensor]
 
         pipeline = self.create_pipeline()
         self.device.startPipeline(pipeline)
@@ -392,12 +411,12 @@ class Main:
                     cam_node.initialControl.setLumaDenoise(0)
                     cam_node.initialControl.setChromaDenoise(4)
 
-                if cam_info['hasAutofocus']:
-                    cam_node.initialControl.setManualFocus(self.focus_value)
+                # if cam_info['hasAutofocus']:
+                cam_node.initialControl.setManualFocus(self.focus_value)
 
-                    controlIn = pipeline.createXLinkIn()
-                    controlIn.setStreamName(cam_info['name'] + '-control')
-                    controlIn.out.link(cam_node.inputControl)
+                controlIn = pipeline.createXLinkIn()
+                controlIn.setStreamName(cam_info['name'] + '-control')
+                controlIn.out.link(cam_node.inputControl)
             xout.input.setBlocking(False)
             xout.input.setQueueSize(1)
 
@@ -897,8 +916,8 @@ class Main:
                 calibration_handler.setCameraIntrinsics(stringToCam[camera], cam_info['intrinsics'],  cam_info['size'][0], cam_info['size'][1])
                 calibration_handler.setFov(stringToCam[camera], cam_info['hfov'])
 
-                if cam_info['hasAutofocus']:
-                    calibration_handler.setLensPosition(stringToCam[camera], self.focus_value)
+                # if cam_info['hasAutofocus']:
+                calibration_handler.setLensPosition(stringToCam[camera], self.focus_value)
 
                 # log_list.append(self.focusSigma[cam_info['name']])
                 # log_list.append(cam_info['reprojection_error'])
