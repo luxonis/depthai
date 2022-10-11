a67d78b9fb1338737bb2214b259a473d98078249
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-10-11 17:12:08 +0200

    Update examples/imu.py

--- depthai_sdk/examples/imu.py
+++ depthai_sdk/examples/imu.py
@@ -9,5 +9,7 @@ with OakCamera() as oak:
     imu = oak.create_imu()
     imu.config_imu(report_rate=400, batch_report_threshold=5)
 
-    oak.callback(imu.out.main, callback=callback)
+    # oak.callback(imu.out.main, callback=callback)
+    oak.visualize(imu.out.main)
+
     oak.start(blocking=True)
