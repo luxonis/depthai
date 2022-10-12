3dd0c034efc489f5a90d0ccbfdcc8abf5d62ad88
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-10-13 00:44:52 +0200

    Update examples/stereo.py

--- depthai_sdk/examples/stereo.py
+++ depthai_sdk/examples/stereo.py
@@ -1,7 +1,9 @@
+import depthai
+
 from depthai_sdk import OakCamera
 
-with OakCamera() as oak:
-    stereo = oak.create_stereo('800p', fps=60)
+with OakCamera(usbSpeed=depthai.UsbSpeed.HIGH) as oak:
+    stereo = oak.create_stereo('400p', fps=60, clickable=True)
     stereo.configure_postprocessing(
         colorize=True,
         wls_filter=True,
@@ -9,5 +11,5 @@ with OakCamera() as oak:
         wls_sigma=1.5
     )
 
-    oak.visualize(stereo.out.disparity)
+    oak.visualize(stereo.out.depth)
     oak.start(blocking=True)
