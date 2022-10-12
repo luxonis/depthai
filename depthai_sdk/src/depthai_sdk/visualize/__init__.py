b172da94b84c17f8b1cf824f94e32e24118f1d47
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-10-13 00:40:43 +0200

    Update __init__.py files

--- depthai_sdk/src/depthai_sdk/visualize/__init__.py
+++ depthai_sdk/src/depthai_sdk/visualize/__init__.py
@@ -1,1 +1,4 @@
 from .visualizer import NewVisualizer
+from .configs import BboxStyle, TextPosition
+from .configs import VisConfig, DetectionConfig, TextConfig, TrackingConfig
+from .objects import GenericObject, VisDetections, VisText, VisLine, VisTrail
