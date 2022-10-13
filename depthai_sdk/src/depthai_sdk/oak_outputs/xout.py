049b88b4b1ca358d0cda87dab44716c8c3561e8e
Filip Peška <56216861+peskaf@users.noreply.github.com>
2022-10-13 17:05:16 +0200

    Add record option.

--- depthai_sdk/src/depthai_sdk/oak_outputs/xout.py
+++ depthai_sdk/src/depthai_sdk/oak_outputs/xout.py
@@ -34,10 +34,16 @@ class XoutFrames(XoutBase):
     Single message, no syncing required
     """
     name: str = "Frames"
+    fps: float
     frames: StreamXout
+    _frame_shape: Tuple[int, int] = None
     _scale: Union[None, float, Tuple[int, int]] = None
     _show_fps: bool = False
-    fps: float
+    _recording_path: Union[None, str] = None
+    _video_writer: Union[cv2.VideoWriter, None] = None
+    _fourcc_codec_code = None
+    _frames_buffer: List = []
+    _FRAMES_TO_BUFFER: int = 20
 
     def __init__(self, frames: StreamXout, fps: float = 30):
         self.frames = frames
@@ -47,9 +53,25 @@ class XoutFrames(XoutBase):
     def setup_visualize(self,
                         scale: Union[None, float, Tuple[int, int]] = None,
                         fps: bool = None,
+                        recording_path: str = None,
                         ):
         self._scale = scale
         self._show_fps = fps
+
+        if recording_path:
+            _recording_path = recording_path.split('.')
+            _recording_path[-2] += '_' + self.name.replace(' ', '_').lower()
+            self._recording_path = '.'.join(_recording_path)
+
+            video_format = _recording_path[-1]
+            if video_format == "mp4":
+                self._fourcc_codec_code = cv2.VideoWriter_fourcc(*'mp4v')
+            elif video_format =="avi":
+                self._fourcc_codec_code = cv2.VideoWriter_fourcc(*'FMP4')
+            else:
+                print("Selected video format not supported, using mp4 instead.")
+                self._fourcc_codec_code = cv2.VideoWriter_fourcc(*'mp4v')
+
         self._vis = True
 
     def visualize(self, packet: FramePacket) -> None:
@@ -60,19 +82,37 @@ class XoutFrames(XoutBase):
         if self._show_fps:
             Visualizer.putText(packet.frame, "FPS: {:.1f}".format(self._fps.fps()), (10, 20), scale=0.7)
 
+        if not self._frame_shape:
+            if self._scale:
+                if isinstance(self._scale, Tuple):
+                    self._frame_shape = self._scale
+                elif isinstance(self._scale, float):
+                    self._frame_shape = (
+                        int(packet.frame.shape[1] * self._scale),
+                        int(packet.frame.shape[0] * self._scale)
+                    )
+            else:
+                self._frame_shape = (packet.frame.shape[1], packet.frame.shape[0])
+
+        # Resize frame
         if self._scale:
-            if isinstance(self._scale, Tuple):
-                packet.frame = cv2.resize(packet.frame, self._scale)  # Resize frame
-            elif isinstance(self._scale, float):
-                packet.frame = cv2.resize(packet.frame, (
-                    int(packet.frame.shape[1] * self._scale),
-                    int(packet.frame.shape[0] * self._scale)
-                ))
+            packet.frame = cv2.resize(packet.frame, self._frame_shape)  
+
+        if self._recording_path and not self._video_writer:
+            if len(self._frames_buffer) < self._FRAMES_TO_BUFFER:
+                self._frames_buffer.append(packet.frame)
+            else:
+                self._video_writer = cv2.VideoWriter(self._recording_path, self._fourcc_codec_code, self._fps.fps(), self._frame_shape)
+                # write all buffered frames
+                for frame in self._frames_buffer:
+                    self._video_writer.write(frame)
 
         if self.callback:  # Don't display frame, call the callback
             self.callback(packet)
         else:
             cv2.imshow(self.name, packet.frame)
+            if self._video_writer:
+                self._video_writer.write(packet.frame)
 
     def xstreams(self) -> List[StreamXout]:
         return [self.frames]
@@ -87,6 +127,9 @@ class XoutFrames(XoutBase):
 
         self.queue.put(packet, block=False)
 
+    def __del__(self):
+        if self._video_writer:
+            self._video_writer.release()
 
 class XoutMjpeg(XoutFrames):
     name: str = "MJPEG Stream"
