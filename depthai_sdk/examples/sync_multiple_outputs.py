31cb2e157a9a104452dbd08972027f52156374d9
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-10-19 16:16:06 +0200

    Update examples/sync_multiple_outputs.py

--- depthai_sdk/examples/sync_multiple_outputs.py
+++ depthai_sdk/examples/sync_multiple_outputs.py
@@ -8,10 +8,13 @@ with OakCamera() as oak:
     nn2 = oak.create_nn('face-detection-retail-0004', color)
     # oak.visualize([nn.out.main, nn.out.passthrough])
     # oak.visualize(nn.out.spatials, scale=1 / 2)
+
     def cb(msgs: Dict):
-        print(msgs)
+        print(msgs, '\n---')
 
     oak.sync([color.out.encoded, nn.out.main, nn2.out.main], cb)
     # oak.show_graph()
+
+    print(color.out.id, nn.out.id, nn2.out.id)
 
     oak.start(blocking=True)
