c6765f06d26aa63ee2f90c039efdbfa022ac0321
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-01-06 21:19:42 +0100

    Update StereoComponent/stereo.py example

--- depthai_sdk/examples/StereoComponent/stereo.py
+++ depthai_sdk/examples/StereoComponent/stereo.py
@@ -5,15 +5,13 @@ from depthai_sdk.components.stereo_component import WLSLevel
 from depthai_sdk.visualize.configs import StereoColor
 
 with OakCamera() as oak:
-    stereo = oak.create_stereo('400p', fps=30)
+    stereo = oak.create_stereo('800p', fps=30)
     stereo.config_postprocessing(
         colorize=StereoColor.GRAY,
         colormap=cv2.COLORMAP_BONE,
         wls_filter=True,
-        wls_level=WLSLevel.MEDIUM,
-        wls_lambda=8000,
-        wls_sigma=1.5
+        wls_level=WLSLevel.HIGH
     )
 
-    oak.visualize(stereo.out.depth)
+    oak.visualize(stereo.out.disparity)
     oak.start(blocking=True)
