6ef4cafc35c79adc48ecb33d7678118f6894962d
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-01-10 03:12:11 +0100

    Lower lambda values for low and medium WLSLevels

--- depthai_sdk/src/depthai_sdk/components/stereo_component.py
+++ depthai_sdk/src/depthai_sdk/components/stereo_component.py
@@ -14,8 +14,8 @@ from depthai_sdk.visualize.configs import StereoColor
 
 class WLSLevel(Enum):
     """WLS filter level"""
-    LOW = (2000, 0.8)
-    MEDIUM = (8000, 1.5)
+    LOW = (1000, 0.8)
+    MEDIUM = (6000, 1.5)
     HIGH = (12000, 2.0)
 
 
