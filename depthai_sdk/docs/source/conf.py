1e0f24378d184379102009bcd76d0f4042bfd855
Erol444 <erol123444@gmail.com>
2023-01-15 02:46:55 +0100

    Update conf.py

--- depthai_sdk/docs/source/conf.py
+++ depthai_sdk/docs/source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'DepthAI SDK Docs'
-copyright = '2021, Luxonis'
+copyright = u"@build_year@, Luxonis"
 author = 'Luxonis'
 
 # The full version, including alpha/beta/rc tags
