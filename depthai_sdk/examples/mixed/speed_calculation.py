074f5902c7dd960b67fe4642d26c00742af68d64
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-01-17 17:29:40 +0100

    Update speed_calculation.py

--- depthai_sdk/examples/mixed/speed_calculation.py
+++ depthai_sdk/examples/mixed/speed_calculation.py
@@ -2,8 +2,14 @@ from depthai_sdk import OakCamera
 
 with OakCamera() as oak:
     color = oak.create_camera('color')
-    nn = oak.create_nn('mobilenet-ssd', color, spatial=True, tracker=True)
+    left = oak.create_camera('left')
+    right = oak.create_camera('right')
+    stereo = oak.create_stereo('800p', left=left, right=right)
+    stereo.config_stereo(subpixel=False, lrCheck=True)
+
+    nn = oak.create_nn('mobilenet-ssd', color, spatial=stereo, tracker=True)
     nn.config_nn(aspect_ratio_resize_mode='stretch')
+
     visualizer = oak.visualize([nn.out.tracker], fps=True)
     visualizer.tracking(speed=True)
 
