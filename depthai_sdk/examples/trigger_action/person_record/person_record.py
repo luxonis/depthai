28d696876c53fed540ed397cdf9d0557a140739c
vviktorrK <korotynskiy.viktor@gmail.com>
2023-01-27 16:14:02 +0100

    shorten variable names

--- depthai_sdk/examples/trigger_action/person_record/person_record.py
+++ depthai_sdk/examples/trigger_action/person_record/person_record.py
@@ -1,20 +1,12 @@
-from depthai_sdk import OakCamera, DetectionPacket
+from depthai_sdk import OakCamera
+from depthai_sdk.trigger_action.triggers.detection_trigger import DetectionTrigger
 from depthai_sdk.trigger_action.actions.record_action import RecordAction
-from depthai_sdk.trigger_action.triggers.abstract_trigger import Trigger
-
-
-def trigger(packet: DetectionPacket) -> bool:
-    dets = packet.img_detections.detections
-    for det in dets:
-        if det.label == 15:  # Person
-            return True
-    return False
 
 
 with OakCamera() as oak:
     color = oak.create_camera('color')
     nn = oak.create_nn('mobilenet-ssd', color)
+    oak.trigger_action(trigger=DetectionTrigger(input=nn, min_detections={'PERSON': 1}, cooldown=30),
+                       action=RecordAction(input=color, path='./', duration_before_trigger=5, duration_after_trigger=10))
     oak.visualize(nn)
-    oak.trigger_action(trigger=Trigger(input=nn, condition=trigger, period=30),
-                       action=RecordAction(input=color, path='./', duration=(5, 10)))
     oak.start(blocking=True)
--- depthai_sdk/src/depthai_sdk/trigger_action/controllers.py
+++ depthai_sdk/src/depthai_sdk/trigger_action/controllers.py
@@ -16,7 +16,7 @@ class TriggerActionController(ABC):
     def __init__(self, trigger: Trigger):
         self.last_trigger_time = datetime.min
         self.trigger_condition = trigger.condition
-        self.trigger_period = trigger.period
+        self.trigger_cooldown = trigger.cooldown
 
     @abstractmethod
     def start(self, device: dai.Device, xout: 'XoutFrames'):  # do we need device here?
@@ -35,7 +35,8 @@ class RecordController(TriggerActionController):
     def __init__(self, trigger: Trigger, action: RecordAction):
         super().__init__(trigger)
         self.path = action.path
-        self.duration = action.duration
+        self.duration_bt = action.duration_before_trigger
+        self.duration_at = action.duration_after_trigger
         self.recorder = VideoRecorder()
         self.stream_name = ''  # will be assigned during recorder setup... TODO: is it OK?
         self.buffers_status: Dict[str, Dict[str, Union[int, List[int]]]] = {}
@@ -43,91 +44,86 @@ class RecordController(TriggerActionController):
 
     def _run(self):
         while True:
-            ready_before_ids = self.buffers_status['ready']['before_trigger']
+            rbt_ids = self.buffers_status['ready']['bt']
 
-            # Wait until there is at least one ready before_trigger buffer
-            while not len(ready_before_ids):
+            # Wait until there is at least one ready before trigger buffer
+            while not len(rbt_ids):
                 time.sleep(0.01)
-            ready_before_id = ready_before_ids.pop(0)
+            rbt_id = rbt_ids.pop(0)
 
             # Create cv2.VideoWriter file
-            buf_name = f'before_trigger_{ready_before_id}'
+            buf_name = f'bt_{rbt_id}'
             subfolder = f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
-            file = self.recorder.create_file(wr_name=self.stream_name,
-                                             subfolder=subfolder,
-                                             filename=self.stream_name,
-                                             buf_name=buf_name)  # buf_name is needed to get its frames' properties
+            self.recorder.create_file(wr_name=self.stream_name,
+                                   subfolder=subfolder,
+                                   buf_name=buf_name)
 
-            # Write before_trigger buffer into file
+            # Write before trigger buffer into file
             while not self.recorder.is_buffer_empty(self.stream_name, buf_name):
-                self.recorder.write_to_file(wr_name=self.stream_name,
-                                            buf_name=buf_name,
-                                            file=file)
+                self.recorder.write_from_buffer(wr_name=self.stream_name,
+                                                buf_name=buf_name,
+                                                n_elems=1)
                 time.sleep(0.01)
 
-            # Wait until after_trigger buffer with the same id is ready
-            while ready_before_id not in self.buffers_status['ready']['after_trigger']:
+            # Wait until after trigger buffer with the same id is ready
+            while rbt_id not in self.buffers_status['ready']['at']:
                 time.sleep(0.01)
 
-            # Write after_trigger buffer into the same file
-            buf_name = f'after_trigger_{ready_before_id}'
+            # Write after trigger buffer into the same file
+            buf_name = f'at_{rbt_id}'
             while not self.recorder.is_buffer_empty(self.stream_name, buf_name):
-                self.recorder.write_to_file(wr_name=self.stream_name,
-                                            buf_name=buf_name,
-                                            file=file)
+                self.recorder.write_from_buffer(wr_name=self.stream_name,
+                                                buf_name=buf_name,
+                                                n_elems=1)
                 time.sleep(0.01)
 
             # Close file
             print(f'Saved to {str(self.path / subfolder)}')
-            file.release()
+            self.recorder.close_writer(self.stream_name)
 
     def new_packet_trigger(self, packet: FramePacket, _=None):  # visualizer seems redundant here
         if self.trigger_condition(packet):
             trigger_time = datetime.now()
-            if trigger_time - self.last_trigger_time > self.trigger_period:
+            if trigger_time - self.last_trigger_time > self.trigger_cooldown:
                 print("TRIGGERED!")
                 self.last_trigger_time = trigger_time
 
                 # Setup for the current recording
-                writing_before_id = self.buffers_status['writing']['before_trigger']
-                self.buffers_status['ready']['before_trigger'].append(writing_before_id)
-                self.buffers_status['writing']['after_trigger'].append(writing_before_id)
+                wbt_id = self.buffers_status['writing']['bt']
+                self.buffers_status['ready']['bt'].append(wbt_id)
+                self.buffers_status['writing']['at'].append(wbt_id)
 
                 # Setup for the next recording
-                new_buf_id = writing_before_id + 1
-                buffers = {f'before_trigger_{new_buf_id}': self.duration[0],
-                           f'after_trigger_{new_buf_id}': self.duration[1]}
+                new_id = wbt_id + 1
+                buffers = {f'bt_{new_id}': self.duration_bt,
+                           f'at_{new_id}': self.duration_at}
                 self.recorder.init_buffers(self.stream_name, buffers)
-                self.buffers_status['writing']['before_trigger'] = new_buf_id
+                self.buffers_status['writing']['bt'] = new_id
 
     def new_packet_action(self, packet: FramePacket, _=None):  # visualizer seems redundant here
-        # Write into the only writing before_trigger buffer
-        buffer_before_id = self.buffers_status['writing']['before_trigger']
-        self.recorder.add_to_buffer(self.stream_name, f'before_trigger_{buffer_before_id}', packet.frame)
+        # Write into the only writing before trigger buffer
+        wbt_id = self.buffers_status['writing']['bt']
+        self.recorder.add_to_buffer(self.stream_name, f'bt_{wbt_id}', packet.frame)
 
-        # Write into all writing after_trigger buffers
-        buffer_after_ids = self.buffers_status['writing']['after_trigger']
-        for i, buffer_id in enumerate(list(buffer_after_ids)):
-            buffer = f'after_trigger_{buffer_id}'
+        # Write into all writing after trigger buffers
+        wat_ids = self.buffers_status['writing']['at']
+        for i, buffer_id in enumerate(list(wat_ids)):
+            buffer = f'at_{buffer_id}'
             if not self.recorder.is_buffer_full(self.stream_name, buffer):
                 self.recorder.add_to_buffer(self.stream_name, buffer, packet.frame)
             else:
-                buffer_after_ids.pop(i)
-                self.buffers_status['ready']['after_trigger'].append(buffer_id)
+                wat_ids.pop(i)
+                self.buffers_status['ready']['at'].append(buffer_id)
 
     def start(self, device: dai.Device, xout: 'XoutFrames'):  # probably extend to multiple xouts
-        self.stream_name = xout.name  # what's the difference between xout.name and xout.frames.name?
-
-        # Check what are the names
-        print('xout.name = ', xout.name)
-        print('xout.frames.name = ', xout.frames.name)
+        self.stream_name = xout.name
 
         self.recorder.update(self.path, device, [xout])  # creates video_writers
 
-        buffers = {'before_trigger_1': self.duration[0], 'after_trigger_1': self.duration[1]}
+        buffers = {'bt_1': self.duration_bt, 'at_1': self.duration_at}
         self.recorder.init_buffers(self.stream_name, buffers)
-        self.buffers_status = {'writing': {'before_trigger': 1, 'after_trigger': []},
-                               'ready': {'before_trigger': [], 'after_trigger': []}}
+        self.buffers_status = {'writing': {'bt': 1, 'at': []},  # there is always only one writing before trigger buffer
+                               'ready': {'bt': [], 'at': []}}
 
         process = Thread(target=self._run)
         process.start()
