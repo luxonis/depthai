a3c82b80f95e2c661404ef6a6e69ade79eb3ab9b
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-02-03 17:09:12 +0100

    Improve imports

--- depthai_sdk/src/depthai_sdk/__init__.py
+++ depthai_sdk/src/depthai_sdk/__init__.py
@@ -2,3 +2,5 @@ from depthai_sdk.args_parser import ArgsParser
 from depthai_sdk.components.nn_helper import ResizeMode
 from depthai_sdk.oak_camera import OakCamera
 from depthai_sdk.oak_device import OakDevice
+from depthai_sdk.replay import *
+from depthai_sdk.record import *
