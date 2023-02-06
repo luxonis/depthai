b66c1bb189d659a663513287bb1d1179b21d1fee
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-02-06 23:38:58 +0100

    Add utils and previews imports

--- depthai_sdk/src/depthai_sdk/__init__.py
+++ depthai_sdk/src/depthai_sdk/__init__.py
@@ -4,3 +4,5 @@ from depthai_sdk.oak_camera import OakCamera
 from depthai_sdk.oak_device import OakDevice
 from depthai_sdk.replay import *
 from depthai_sdk.record import *
+from depthai_sdk.previews import *
+from depthai_sdk.utils import *
