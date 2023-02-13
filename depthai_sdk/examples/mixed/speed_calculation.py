c47b2444ddc1892e054f7a13121ed5223af7eff6
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-02-13 03:34:59 +0100

    Update speed_calculation.py

--- depthai_sdk/examples/mixed/speed_calculation.py
+++ depthai_sdk/examples/mixed/speed_calculation.py
@@ -1,16 +1,25 @@
+import cv2
+
 from depthai_sdk import OakCamera
+
+
+def callback(packet):
+    for detection in packet.detections:
+        print(f'Speed: {detection.speed:.02f} m/s, {detection.speed_kmph:.02f} km/h, {detection.speed_mph:.02f} mph')
+
+    frame = packet.visualizer.draw(packet.frame)
+    cv2.imshow('Speed estimation', frame)
+
 
 with OakCamera() as oak:
     color = oak.create_camera('color')
-    left = oak.create_camera('left')
-    right = oak.create_camera('right')
-    stereo = oak.create_stereo('800p', left=left, right=right)
-    stereo.config_stereo(subpixel=False, lrCheck=True)
+    stereo = oak.create_stereo('800p')
+    stereo.config_stereo(subpixel=False, lr_check=True)
 
-    nn = oak.create_nn('mobilenet-ssd', color, spatial=stereo, tracker=True)
-    nn.config_nn(aspect_ratio_resize_mode='stretch')
+    nn = oak.create_nn('face-detection-retail-0004', color, spatial=stereo, tracker=True)
+    nn.config_nn(resize_mode='stretch')
 
-    visualizer = oak.visualize([nn.out.tracker], fps=True)
-    visualizer.tracking(speed=True)
+    visualizer = oak.visualize(nn.out.tracker, callback=callback)
+    visualizer.tracking(speed=True).text(auto_scale=False)
 
     oak.start(blocking=True)
