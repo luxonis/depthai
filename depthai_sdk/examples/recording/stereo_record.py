a63b1e21865af486d424f35e1e171ad4ec06406f
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-03-08 20:35:56 +0100

    Update stereo_record.py example

--- depthai_sdk/examples/recording/stereo_record.py
+++ depthai_sdk/examples/recording/stereo_record.py
@@ -9,10 +9,11 @@ with OakCamera() as oak:
 
     stereo.config_postprocessing(
         colorize=StereoColor.RGB,
-        colormap=cv2.COLORMAP_JET,
-        wls_filter=True,
-        wls_lambda=8000,
-        wls_sigma=1.5
+        colormap=cv2.COLORMAP_JET
+    )
+
+    stereo.config_wls(
+        wls_level='high'  # options: 'low', 'medium', 'high'
     )
 
     # Record RGB and disparity to records folder
