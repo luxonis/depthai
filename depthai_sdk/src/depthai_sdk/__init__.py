72ba211c0ba6e233f657b6e502d678ef5a5d792e
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-05-01 14:38:22 +0200

    Add sentry.io reporting, add CLI command depthai-sdk

--- depthai_sdk/src/depthai_sdk/__init__.py
+++ depthai_sdk/src/depthai_sdk/__init__.py
@@ -7,4 +7,36 @@ from depthai_sdk.previews import *
 from depthai_sdk.record import *
 from depthai_sdk.replay import *
 from depthai_sdk.utils import *
+from depthai_sdk.utils import _create_config, get_config_field
 from depthai_sdk.visualize import *
+
+__version__ = '1.9.6'
+
+
+def __import_sentry(sentry_dsn: str) -> None:
+    try:
+        import sentry_sdk
+
+        sentry_sdk.init(
+            dsn=sentry_dsn,
+            traces_sample_rate=1.0,
+            release='depthai_sdk@1.9.5',
+            with_locals=False,
+        )
+    except:
+        pass
+
+
+config_exists = False
+# Check if sentry is enabled
+try:
+    sentry_status = get_config_field('sentry')
+    config_exists = True
+except FileNotFoundError:
+    sentry_status = False
+
+if config_exists and sentry_status:
+    sentry_dsn = get_config_field('sentry_dsn')
+    __import_sentry(sentry_dsn)
+elif not config_exists:
+    _create_config()
