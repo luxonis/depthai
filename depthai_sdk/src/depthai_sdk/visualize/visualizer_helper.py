eb39af40d8d173f5b879af192b2dbffea96eb0ee
Erol444 <erol123444@gmail.com>
2023-05-14 20:31:13 +0200

    Remove distinctipy as it's slow, takes 2.5sec at startup to generate coco80 colors

--- depthai_sdk/src/depthai_sdk/visualize/visualizer_helper.py
+++ depthai_sdk/src/depthai_sdk/visualize/visualizer_helper.py
@@ -9,7 +9,6 @@ except ImportError:
     cv2 = None
 
 import depthai as dai
-import distinctipy
 import numpy as np
 
 from depthai_sdk.classes.packets import (
@@ -19,8 +18,7 @@ from depthai_sdk.classes.packets import (
     TrackerPacket,
     _TrackingDetection
 )
-from depthai_sdk.oak_outputs.normalize_bb import NormalizeBoundingBox
-
+from depthai_sdk.visualize.bbox import BoundingBox
 
 class FramePosition(IntEnum):
     """
@@ -240,14 +238,6 @@ def rectangle(src,
 
     return src
 
-
-def get_text_color(background, threshold=0.6):
-    bck = np.array(background) / 256
-    clr = distinctipy.get_text_color((bck[2], bck[1], bck[0]), threshold)
-    clr = distinctipy.get_rgb256(clr)
-    return (clr[2], clr[1], clr[0])
-
-
 def draw_mappings(packet: SpatialBbMappingPacket):
     dets = packet.spatials.detections
     for det in dets:
@@ -273,7 +263,7 @@ def spatials_text(spatials: dai.Point3f):
 
 
 def draw_detections(packet: Union[DetectionPacket, _TwoStageDetection, TrackerPacket],
-                    norm: NormalizeBoundingBox,
+                    norm: BoundingBox,
                     label_map: List[Tuple[str, Tuple]] = None):
     """
     Draw object detections to the frame.
@@ -290,7 +280,8 @@ def draw_detections(packet: Union[DetectionPacket, _TwoStageDetection, TrackerPa
         img_detections = [det for det in packet.img_detections.detections]
 
     for detection in img_detections:
-        bbox = norm.normalize(packet.frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+        tl,br = norm.get_relative_bbox(BoundingBox(detection)).map_to_frame(packet.frame.shape)
+        bbox = [*tl, *br]
 
         if label_map:
             txt, color = label_map[detection.label]
