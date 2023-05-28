85a1a713bf47d7fddb48b1a163713e356d2765a6
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-05-28 16:14:09 +0200

    Rename get_sharpness to sharpness

--- depthai_sdk/src/depthai_sdk/evaluate.py
+++ depthai_sdk/src/depthai_sdk/evaluate.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def get_sharpness(frame: np.ndarray):
+def sharpness(frame: np.ndarray):
     dx = np.diff(frame)[1:, :]  # remove the first row
     dy = np.diff(frame, axis=0)[:, 1:]  # remove the first column
     dnorm = np.sqrt(dx ** 2 + dy ** 2)
